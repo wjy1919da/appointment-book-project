d76c0056391951c98523dbe8c9c300b2420d8a6e
minmincc <107015634+minmincc@users.noreply.github.com>
2023-06-02 14:55:07 -0700

    add community and community-card component

---
+++ src/components/community-post/community-post.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import "./community-post.styles.scss";
+import heartIcon from "../../assets/doctor/heartIcon.png"
+
+
+const CommunityPost = ({imageURL,text,profileImage,authorName,likes}) => {
+
+    return (
+        <div className='community-post-container'>
+            {/* <img src={imageURL} alt="postImage" className="post-Image"></img> */}
+            <div classname="post-Image">
+                <img src={imageURL} className="post-image-url"></img>
+            </div>
+            <div className="post-information">
+                <span className="post-text">{text}</span>
+                <div className="profile">
+                    <div className="profileImage">
+                        <img src={profileImage}></img>
+                        <span className="gray-text">{authorName}</span>
+                    </div>
+                    <div className="likeNumber">
+                       <img src={heartIcon} className="heartIcon"></img>
+                        <span className="gray-text" style={{marginTop:"-8px"}}>{likes}</span>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CommunityPost;
