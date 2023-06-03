4143e180e5f751f7323ba8583508846d1bfc9609
jiaqima1110 <jiaqima@usc.edu>
2023-06-02 19:02:24 -0700

    更改search button样式

--- src/components/doctor-search-button/doctor-search-button.component.jsx
+++ src/components/doctor-search-button/doctor-search-button.component.jsx
@@ -2,9 +2,13 @@ import './doctor-search-button.styles.scss'
 import SearchIcon from '../../assets/doctor/doctor-search-button-icon.png';
 const DoctorSearchButton = ({title, onClick}) => {
     const handleClick = () => {onClick()};
-    return <button className='doctor-search-button' onClick={handleClick}>
-        <img src={SearchIcon} alt="Search" className='doctor-search-icon' />
-        {title}
-    </button>;
+    return (
+        <div className='doctor-search-button-container'>
+            <button className='doctor-search-button' onClick={handleClick}>
+                <img src={SearchIcon} alt="Search" className='doctor-search-icon' />
+                {title}
+            </button>
+        </div>
+    )
 };
 export default DoctorSearchButton;
