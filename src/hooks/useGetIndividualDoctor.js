1dc94074dd3361bf2f0e7df6d8ef0bb5830f8ec8
jiaqima1110 <jiaqima@usc.edu>
2023-06-23 17:47:03 -0700

    Add useGetDoctorAbout() API

--- src/hooks/useGetIndividualDoctor.js
+++ src/hooks/useGetIndividualDoctor.js
@@ -37,3 +37,39 @@ export function useGetDoctorReviews() {
       }
     );
 }
+
+export function useGetDoctorAbout() {
+  const doctorQuery = useDoctorQueryStore((state) => state.doctorQuery);
+  console.log(doctorQuery)
+  const clearnickName=doctorQuery.nickName.replace(":", "")
+  const fetchDoctorReviews = async ({ pageParam = 1 }) => {
+    try {
+      const response = await axios.post(
+        'http://api.charm-life.com/info/doctor-details',
+        {
+          "currentPage": pageParam,
+          "memberId": 45,
+          "nickname": clearnickName,
+          "pageSize": doctorQuery.pageSize,
+
+        }
+      );
+
+      console.log("doctor about data",response.data);
+      return { data: response.data.data, pageInfo: response.data.pageInfo };
+    } catch (error) {
+      throw new Error('Failed to fetch doctor about');
+    }
+  };
+
+  return useInfiniteQuery(
+    ['doctor-about', doctorQuery.nickName, doctorQuery.pageSize],
+    fetchDoctorReviews,
+    {
+      staleTime: 1 * 6 * 1000 * 60 * 3, // 3 hours
+      keepPreviousData: true,
+      getNextPageParam: (lastPage, allPages) =>
+      lastPage.data.length > 0 ? allPages.length + 1 : undefined,
+    }
+  );
+}
