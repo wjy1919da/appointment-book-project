a06dd48f4af5c48fd96301c8756448f65dad7d4f
wjy1919da <wjy1917886@gmail.com>
2023-08-29 20:56:05 -0400

    update download page
    update download page

--- src/routes/download/download.component.jsx
+++ src/routes/download/download.component.jsx
@@ -5,34 +5,38 @@ import QRiOS from '../../assets/download/QR-code-iOS.png';
 import BadgeiOS from '../../assets/download/iOS-Badges.png';
 import QRAndroid from '../../assets/download/QR-code-Android.png';
 import BadgeAndroid from '../../assets/download/Android-Badges.png';
+import Footer from '../../components/footer/footer.component';
 
 const Download = () => {
     
 
     return (
-        <div className='download-div'>
-            <div className='download-pics'>
-                <div className='decorator'></div>
-                <img className='phone-pic' src={iPhone} alt='iphone'></img>
-            </div>
-            <div className='QR-codes-div'>
-                <p className='QR-title'>Get the Charm App</p>
-                <p className='QR-subtitle'>Your Charming Life Starts Here</p>
-                <div className='QR-codes'>
-                    <div className='QR-code'>
-                        <img className='QR-code-pic' src={QRiOS} alt='QR-ios'></img>
-                        <Link className='download-link'>
-                            <img className='download-btn' src={BadgeiOS} alt='download-ios'></img>
-                        </Link>
+        <div className='download-container'>
+            <div className='download-div'>
+                    <div className='download-pics'>
+                        <div className='decorator'></div>
+                        <img className='phone-pic' src={iPhone} alt='iphone'></img>
                     </div>
-                    <div className='QR-code'>
-                        <img className='QR-code-pic' src={QRAndroid} alt='QR-android'></img>
-                        <Link className='download-link'>
-                            <img className='download-btn' src={BadgeAndroid} alt='download-android'></img>
-                        </Link>
+                    <div className='QR-codes-div'>
+                        <p className='QR-title'>Get the Charm App</p>
+                        <p className='QR-subtitle'>Your Charming Life Starts Here</p>
+                        <div className='QR-codes'>
+                            <div className='QR-code'>
+                                <img className='QR-code-pic' src={QRiOS} alt='QR-ios'></img>
+                                <Link className='download-link'>
+                                    <img className='download-btn' src={BadgeiOS} alt='download-ios'></img>
+                                </Link>
+                            </div>
+                            <div className='QR-code'>
+                                <img className='QR-code-pic' src={QRAndroid} alt='QR-android'></img>
+                                <Link className='download-link'>
+                                    <img className='download-btn' src={BadgeAndroid} alt='download-android'></img>
+                                </Link>
+                            </div>
+                        </div>
                     </div>
-                </div>
             </div>
+            <Footer/>
         </div>
     )
 }
