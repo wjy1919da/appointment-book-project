c0415526de20fa0dbba9ff08050abdc0f8a5be48
wjy1919da <wjy1917886@gmail.com>
2023-09-08 17:40:48 -0400

    save
    save

--- src/components/recommendation-grid/recommendation-grid.component.jsx
+++ src/components/recommendation-grid/recommendation-grid.component.jsx
@@ -16,14 +16,14 @@ const RecommendationGrid = ({ style }) => {
           address: "456 Wellness Ave, Health City",
           name: "Sarah Johnson",
           mechName: "Wellness Clinic",
-        },
-        {
-          id: 3,
-          nickname: "Dr. Lee",
-          address: "789 Health Blvd, Health City",
-          name: "", // 这个医生没有名字
-          mechName: "Life Care Center",
         }
+        // {
+        //   id: 3,
+        //   nickname: "Dr. Lee",
+        //   address: "789 Health Blvd, Health City",
+        //   name: "", // 这个医生没有名字
+        //   mechName: "Life Care Center",
+        // }
         // {
         //   id: 4,
         //   nickname: "Dr. Patel",
