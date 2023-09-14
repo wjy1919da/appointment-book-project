a8d7c56ef2f311c688059b38e601114823bd5e45
wjy1919da <wjy1917886@gmail.com>
2023-09-14 09:29:08 -0400

    update hook and doctor page
    update hook and home-title issue

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -18,3 +18,17 @@ export function useUserLogin() {
         placeholderData: { data: {} }, // Default object to use before fetching completes
     });
 }
+
+export function useUserRegister() {
+    const userInfo = userInfoStore(s => s.userInfo);
+    const fetchUserRegister = async () => {
+        const res = await axios.post(base.userLoginUrl, {
+            "email": userInfo.email,
+            "password": userInfo.password,
+        });
+        return res.data;
+    };
+    return useQuery(['userRegister', userInfo.email, userInfo.password], fetchUserRegister, {
+        placeholderData: { data: {} }, // Default object to use before fetching completes
+    });
+}
