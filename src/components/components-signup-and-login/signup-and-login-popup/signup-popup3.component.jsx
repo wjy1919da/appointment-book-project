6965351fcf6eee2664c4530da67c2bae4dd7bfbf
psteph29 <paigestephenson29@gmail.com>
2023-10-03 13:24:03 -0600

    Add account type selection

--- src/components/components-signup-and-login/signup-and-login-popup/signup-popup3.component.jsx
+++ src/components/components-signup-and-login/signup-and-login-popup/signup-popup3.component.jsx
@@ -5,12 +5,15 @@ import SignUpForm from './signup-form.component';
 import SignupVerify from './signup-varify.component';
 import SocialSignUP from './social-signup.component';
 import ChooseGender from './choose-gender.component';
-import BirthYearPicker from './birth-year-picker.component';
+// import BirthYearPicker from './birth-year-picker.component';
 import ChooseInterestedArea from './choose-interested-area.component';
 import SignUpFinal from './sign-up-final.component';
 import userInfoQueryStore from '../../../userStore.ts';
 import LoginForm from './login-form.component';
 import SignUpDownloadPopUp from './signup-popUp-4-download';
+
+import SignUpAccountType from './sign-up-account-type.component';
+
 // 注册&登录
 // 主页面
 const StepTracker = ({ currentStep }) => {
@@ -53,15 +56,16 @@ const SignupPopup3 = (props) => {
                         </button>
             </Modal.Header>
             {userInfo.popupState !== 'login' && <StepTracker currentStep={userInfo.popupState} />}
+            {/* {userInfo.popupState === 'accountType' && <SignUpAccountType />} */}
             {userInfo.popupState === 'signUp' && <SignUpForm/>}
             {userInfo.popupState === 'login' && <LoginForm/>}
             {userInfo.popupState === 'verifyEmail' && <SignupVerify />}
             {userInfo.popupState === 'gender' && <ChooseGender/>}
-            {/* {userInfo.popupState === 'birthday' && <BirthYearPicker />} */}
             {userInfo.popupState === 'interest' && <ChooseInterestedArea />}
             {userInfo.popupState === 'download' && <SignUpDownloadPopUp />}
             {userInfo.popupState === 'success' && <SignUpFinal />}
             {(userInfo.popupState === 'signUp' || userInfo.popupState === 'verifyEmail') && <SocialSignUP />}
+
         </Modal>
     )
 }
