947124ac6305023a74007c3275461fad9c262f72
psteph29 <paigestephenson29@gmail.com>
2023-10-03 21:31:18 -0600

    Add buttonfunctionality

--- src/components/components-signup-and-login/signup-and-login-popup/sign-up-account-type.component.jsx
+++ src/components/components-signup-and-login/signup-and-login-popup/sign-up-account-type.component.jsx
@@ -1,22 +1,50 @@
 import React from 'react';
 import { Button } from 'react-bootstrap'; 
 import LoginRegisterTitle from './login-register-title.component'; 
-import './sign-up-account-type.scss'; 
+import './sign-up-account-type.styles.scss'; 
+import userInfoQueryStore from '../../../userStore.ts';
 
 const SignUpAccountType = () => {
+    const setAccountType = userInfoQueryStore((state) => state.setAccountType);
+    const switchPopupTab = userInfoQueryStore(state => state.switchPopupTab);
+
+   
+    console.log(setAccountType);
+
     return (
         <div className="signup-account-type-container">
             <div className="title-container">
                 <LoginRegisterTitle title="Join Charm as a..."/>
             </div>
             <div className="button-container">
-                <Button variant="primary">Doctor</Button>
-                <Button variant="secondary">User</Button>
+                <Button 
+                    variant="primary" 
+                    onClick={() => {
+                        console.log('Doctor button clicked');  
+                        setAccountType('doctor');
+                    }}>
+                    Doctor
+                </Button>
+                <Button 
+                    variant="secondary" 
+                    onClick={() => {
+                        console.log('User button clicked');  
+                        setAccountType('user');
+                    }}>
+                    User
+                </Button>
+                <Button 
+         
+                    onClick={() => {
+                        console.log('User button clicked');  
+                        switchPopupTab('login');
+                    }}>
+                    Next
+                </Button>
+          
             </div>
         </div>
     );
 };
-
-// Commenting to test 
 
 export default SignUpAccountType;
