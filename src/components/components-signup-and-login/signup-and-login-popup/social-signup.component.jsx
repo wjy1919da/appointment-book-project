e6e31618975ad4d70425f553cb049c1865b46cfe
wjy1919da <wjy1917886@gmail.com>
2023-10-06 17:22:59 -0400

    update img
    add img

--- src/components/components-signup-and-login/signup-and-login-popup/social-signup.component.jsx
+++ src/components/components-signup-and-login/signup-and-login-popup/social-signup.component.jsx
@@ -35,49 +35,49 @@ import FacebookLoginBtnWrap from '../signup-and-login-button/sigin-with-gfa-butt
 import GoogleLoginBtnWrap from '../signup-and-login-button/sigin-with-gfa-button-google.component';
 
 const SocialSignUP = ({onHide}) => {
-    const [provider, setProvider] = useState('')
-    const [profile, setProfile] = useState(null)
-    const REDIRECT_URI = 'http://localhost:3000';
-    const setToken = userInfoQueryStore(state=>state.setToken);
-    const onLoginStart = useCallback(() => {
-        alert('login start');
-      }, []);
-    const { mutate, data, isLoading, isError, error } = useSocialLogin();
-    const onResolve = async ({provider, data}) => {
-        if (provider === 'google') {
-          console.log("google ", data);
-          try {
-            const accessToken = data.access_token;
-            // setGoogleToken(accessToken);
-            // onHide();
-            console.log("accessToken ",accessToken);
-            mutate({
-                googleAccessToken: accessToken,
-                provider: 'google'
-            });
-          } catch (error) {
-            console.log('error signing in', error);
-          }
+  //   const [provider, setProvider] = useState('')
+  //   const [profile, setProfile] = useState(null)
+  //   const REDIRECT_URI = 'http://localhost:3000';
+  //   const setToken = userInfoQueryStore(state=>state.setToken);
+  //   const onLoginStart = useCallback(() => {
+  //       alert('login start');
+  //     }, []);
+  //   const { mutate, data, isLoading, isError, error } = useSocialLogin();
+  //   const onResolve = async ({provider, data}) => {
+  //       if (provider === 'google') {
+  //         console.log("google ", data);
+  //         try {
+  //           const accessToken = data.access_token;
+  //           // setGoogleToken(accessToken);
+  //           // onHide();
+  //           console.log("accessToken ",accessToken);
+  //           mutate({
+  //               googleAccessToken: accessToken,
+  //               provider: 'google'
+  //           });
+  //         } catch (error) {
+  //           console.log('error signing in', error);
+  //         }
          
-          // if (data?.data && 400 <= data.code <=500) {
-          //     alert(data.msg);
-          // }
-        } 
-        if (provider === 'facebook') {
-            console.log("facebook "+data);
-        }
+  //         // if (data?.data && 400 <= data.code <=500) {
+  //         //     alert(data.msg);
+  //         // }
+  //       } 
+  //       if (provider === 'facebook') {
+  //           console.log("facebook "+data);
+  //       }
 
-    };  
-    useEffect(() => {
-      // 100 success
-      if (data?.data && data.code === 100) {
-          alert(data.msg);
-          onHide();
-      }
-      if (data?.data && data.code == 400) {
-          alert(data.msg);
-      }
-  }, [data]);
+  //   };  
+  //   useEffect(() => {
+  //     // 100 success
+  //     if (data?.data && data.code === 100) {
+  //         alert(data.msg);
+  //         onHide();
+  //     }
+  //     if (data?.data && data.code == 400) {
+  //         alert(data.msg);
+  //     }
+  // }, [data]);
   return (
         <div className="login-popup-gfa-section">
             <div className="or-section">
