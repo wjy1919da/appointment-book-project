66c3cb2e89864512ccf575d0bf68a00f02fa4c82
psteph29 <paigestephenson29@gmail.com>
2023-10-06 14:08:32 -0600

    Already have an account? Log In

--- src/components/components-signup-and-login/signup-and-login-popup/sign-up-account-type.component.jsx
+++ src/components/components-signup-and-login/signup-and-login-popup/sign-up-account-type.component.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Button } from 'react-bootstrap'; 
 import LoginRegisterTitle from './login-register-title.component'; 
 import './sign-up-account-type.styles.scss'; 
@@ -10,43 +10,58 @@ const SignUpAccountType = () => {
     const setAccountType = userInfoQueryStore((state) => state.setAccountType);
     const switchPopupTab = userInfoQueryStore(state => state.switchPopupTab);
     const userInfo = userInfoQueryStore((state) => state.userInfo);
-    // const [isValid, setIsValid] = useState(false);
-    //console.log('accountType', userInfo.accountType);
+
+    const handleLoginButtonClick = () => {
+        console.log("Login button clicked!");
+        // Add your logic for the login button click event
+    };
+
+    console.log('accountType', userInfo.accountType);
+
     return (
         <div className="signup-account-type-container">
-            <div className="title-container">
-                <LoginRegisterTitle title="Join Charm as a..."/>
-            </div>
-            <div className="button-container">
-                <MemberAndDoctorButton 
-                    title="Doctor"
-                    onClick={() => {
-                        console.log('Doctor button clicked');  
-                        setAccountType('doctor');
-                        // setIsValid(true);
-                        if (userInfo.accountType === 'doctor') {
-                            setAccountType(null);
-                        } else {
+            <div className="group-title-buttons">
+                <div className="title-container">
+                    Join Charm as a...
+                </div>
+                <div className="button-container">
+                    <MemberAndDoctorButton 
+                        title="Doctor"
+                        onClick={() => {
+                            console.log('Doctor button clicked');  
                             setAccountType('doctor');
-                        }
-                    }}
-                     disabled={userInfo.accountType && userInfo.accountType !== 'doctor'}
-                />
-                <MemberAndDoctorButton 
-                    title="Member"
-                    onClick={() => {
-                        console.log('Member button clicked');  
-                        setAccountType('member');
-                        if (userInfo.accountType === 'member') {
-                            setAccountType(null);
-                        } else {
+                            if (userInfo.accountType === 'doctor') {
+                                setAccountType(null);
+                            } else {
+                                setAccountType('doctor');
+                            }
+                        }}
+                        disabled={userInfo.accountType && userInfo.accountType !== 'doctor'}
+                    />
+                    <MemberAndDoctorButton 
+                        title="Member"
+                        onClick={() => {
+                            console.log('Member button clicked');  
                             setAccountType('member');
-                        }
-                    }}
-                     disabled={userInfo.accountType && userInfo.accountType !== 'member'}
-                />
-                <NextButton title="Next" type="submit" disabled={!userInfo.accountType} />
+                            if (userInfo.accountType === 'member') {
+                                setAccountType(null);
+                            } else {
+                                setAccountType('member');
+                            }
+                        }}
+                        disabled={userInfo.accountType && userInfo.accountType !== 'member'}
+                    />
+                </div>
             </div>
+            <NextButton title="Next" type="submit" disabled={!userInfo.accountType} />
+            <div className="login-prompt-container">
+    <span>Already have an account? </span> 
+  
+    <button style={{ color: '#F48C8A', textDecoration: 'none', background: 'none', border: 'none' }} onClick={handleLoginButtonClick}>  Log in</button>
+
+</div>
+
+
         </div>
     );
 };
