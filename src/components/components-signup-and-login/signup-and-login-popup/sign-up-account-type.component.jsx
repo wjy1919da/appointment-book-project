d5c97aa6896ba15482365ac98267bf3d5abc4b4b
wjy1919da <wjy1917886@gmail.com>
2023-10-12 20:19:48 -0400

    commit
    commit

--- src/components/components-signup-and-login/signup-and-login-popup/sign-up-account-type.component.jsx
+++ src/components/components-signup-and-login/signup-and-login-popup/sign-up-account-type.component.jsx
@@ -12,16 +12,15 @@ const SignUpAccountType = () => {
     const userInfo = userInfoQueryStore((state) => state.userInfo);
 
     const handleLoginButtonClick = () => {
-        console.log("Login button clicked!");
+        // console.log("Login button clicked!");
         switchPopupTab('login')
     };
 
-    console.log('accountType', userInfo.accountType);
+    console.log('popupState', userInfo.popupState);
 
     return (
         <div className="signup-account-type-container">
             <div className="group-title-buttons">
-                
                 <div className="title-container">
                     Join Charm as a...
                 </div>
@@ -29,32 +28,32 @@ const SignUpAccountType = () => {
                     <MemberAndDoctorButton 
                         title="Doctor"
                         onClick={() => {
-                            //console.log('Doctor button clicked');  
-                            setAccountType('DOCTOR');
-                            if (userInfo.accountType === 'doctor') {
+                            console.log('Doctor button clicked');  
+                            //setAccountType(0);
+                            if (userInfo.accountType === 2) {
                                 setAccountType(null);
                             } else {
-                                setAccountType('doctor');
+                                setAccountType(2);
                             }
                         }}
-                        disabled={userInfo.accountType && userInfo.accountType !== 'doctor'}
+                        disabled={userInfo.accountType && userInfo.accountType !== 2}
                     />
                     <MemberAndDoctorButton 
                         title="Member"
                         onClick={() => {
-                            //console.log('Member button clicked');  
-                            setAccountType('member');
-                            if (userInfo.accountType === 'member') {
+                            console.log('Member button clicked');  
+                            //setAccountType(0);
+                            if (userInfo.accountType === 1) {
                                 setAccountType(null);
                             } else {
-                                setAccountType('USER');
+                                setAccountType(1);
                             }
                         }}
-                        disabled={userInfo.accountType && userInfo.accountType !== 'member'}
+                        disabled={userInfo.accountType && userInfo.accountType !== 1}
                     />
                 </div>
             </div>
-            <NextButton title="Next" onClick={()=>switchPopupTab('signUp')} disabled={!userInfo.accountType} />
+            <NextButton title="Next" onClick={()=>switchPopupTab('sendVerifyEmail')} disabled={!userInfo.accountType} />
             <div className="login-prompt-container">
                 <span>Already have an account? </span> 
                 <button style={{ color: '#F48C8A', textDecoration: 'none', background: 'none', border: 'none' }} onClick={handleLoginButtonClick}>  Log in</button>
