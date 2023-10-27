8c5c37197a47e34ddd2eb39bc7c7049c26923dea
Eli Bales <elibales21@gmail.com>
2023-10-27 13:04:01 -0400

    removed console logs, correctly connects to backend

--- src/hooks/useContactForm.js
+++ src/hooks/useContactForm.js
@@ -5,17 +5,11 @@ const contactUrl = 'http://api-dev.charm-life.com/user/contact_us';
 export async function ContactSubmission(submissionObj) {
     const sendSubmission = async (data) => {
         try {
-            console.log(`Sending contact form data to ${contactUrl}`);
             const res = await axios.post(contactUrl, data);
-            console.log(`Res returned as: ${res}`);
             return res;
         } catch (error) {
-            console.log(`Error sending contact form data: ${error}`);
             throw new Error('Unable to send data');
         }
     }
     const returnedInfo = await sendSubmission(submissionObj);
-    console.log('Returned info is: ', returnedInfo);
 }
-
-const delay = ms => new Promise(res => setTimeout(res, ms));
