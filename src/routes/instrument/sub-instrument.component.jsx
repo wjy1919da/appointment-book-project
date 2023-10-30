95c154a54f9ce54923f4e194125267e792793711
Eli Bales <elibales21@gmail.com>
2023-10-30 15:45:29 -0400

    (mostly) fixed instrument details page, called correct API

--- src/routes/instrument/sub-instrument.component.jsx
+++ src/routes/instrument/sub-instrument.component.jsx
@@ -18,6 +18,8 @@ import ProcedureCard from '../../components/procedure-card/procedure-card.compon
 import InstrumentFAQ from '../../components/instrument-FQA/instrument-FQA.component'
 import SubProcedureMobileExtraBottom from '../../components/sub-procedure-mobile-extra-bottom/sub-procedure-mobile-extra-bottom.component';
 import useGetProcedures from '../../hooks/useGetProcedures';
+import { useGetInstruments } from '../../hooks/useGetInstruments';
+import { GetDetailedInstruments } from '../../hooks/useGetDetailedInstruments';
 import useProcedureQueryStore from '../../procedureStore.ts'
 function safeJsonParse(str) {
     try {
@@ -28,6 +30,9 @@ function safeJsonParse(str) {
     }
 }
 const SubInstrument = () => {
+    const [data, setData] = useState({});
+    const [subcategories, setSubcategories] = useState([]);
+    const [error, setError] = useState(false);
     const images = {
         coolsculpting,
         inmode,
@@ -38,40 +43,59 @@ const SubInstrument = () => {
     const result = {"optionsForm":[{"header":[{"name":"Workstations"},{"name":"Technologies Covered"},{"name":"Technologies Included"}]},{"body":[{"name":"InMode Forma","value":["Radiofrequency","Radiofrequency for skin tightening"]},{"name":"InMode Evoke","value":["Microneedling","Microneedling for collagen production"]},{"name":"InMode Morpheus8","value":["IPL","IPL for pigmentation and vascular lesions"]},{"name":"InMode Votiva","value":["Diode Laser","Diode Laser for hair removal"]},{"name":"InMode Lumecca","value":["Fractional Coagulation","Fractional Coagulation for skin resurfacing"]},{"name":"InMode DiolazeXL","value":["Temperature Monitoring","Temperature Monitoring for safety"]},{"name":"InMode Triton","value":["Auto-adjusting","Auto-adjusting for custom treatment"]},{"name":"InMode Optimas","value":["Multi-Wavelength","Multi-Wavelength for various treatments"]},{"name":"InMode Avance","value":["Pulse Control","Pulse Control for optimized output"]}]}]};
     const { name } = useParams();
     const setCategories = useProcedureQueryStore(state=>state.setCategories);
-    const { data, isLoading, error } = useGetProcedures();
-    const subcategories = data?.data?.subcategories || [];
+    // const { data, isLoading, error } = useGetProcedures();
+    // const data = GetDetailedInstruments(name);
+    // console.log('Data is: ', data);
+    // let subcategories = data?.data?.data?.subcategories;
+    // console.log('Subcategories is: ', subcategories);
     useEffect(() => {
+        window.scroll(0, 0);
         setCategories(name);
+        const getData = async () => {
+            try {
+                const res = await GetDetailedInstruments(name);
+                console.log('res is: ', res);
+                if (res?.code === 104) throw new Error();
+                setData(res);
+                setSubcategories(res.data.subcategories);
+            } catch (err) {
+                setError(true);
+            }
+        }
+        getData();
     }, [name]);
-    console.log('instrument-data',data);
 
     const imageToUse = images[name];
     function getSubCategory(index) {
-        if (data.data && Array.isArray(data.data.subcategories) && data.data.subcategories.length > index) {
-            return data.data.subcategories[index];
+        if (data && subcategories.length > index) {
+            return subcategories[index];
         }
         return null;
     }
     var prosAndCons, mechanism,optionsContent, potentialAffetc,beforeAndAfterImage, reference, alternativeTreatmentForm, cardInfo;
-    if (data.data && data.data.subcategories) {
+    if (data && subcategories) {
        //console.log('optionsContentdata',data.data.subcategories[2].other)
+       console.log('Creating subcategories!');
        const subCategory1 = getSubCategory(1);
        if (subCategory1) {
-            prosAndCons = subCategory1.explanation ? safeJsonParse(subCategory1.explanation) : undefined;
+            // prosAndCons = subCategory1.explanation ? safeJsonParse(subCategory1.explanation) : undefined;
+            prosAndCons = subCategory1.explanation ? subCategory1.explanation : undefined;
         }
        const subCategory2 = getSubCategory(2);
          if (subCategory2) {
+            //  mechanism = subCategory2.other ? safeJsonParse(subCategory2.other) : undefined;
              mechanism = subCategory2.other ? safeJsonParse(subCategory2.other) : undefined;
               //console.log('optionsContentdata',optionsContent)
           }
         const subCategory3 = getSubCategory(3);
         if (subCategory3) {
-            optionsContent = subCategory3.other ? safeJsonParse(subCategory2.other) : undefined;
-             
+            // optionsContent = subCategory3.other ? safeJsonParse(subCategory2.other) : undefined;
+            optionsContent = subCategory3.other ? safeJsonParse(subCategory3.other) : undefined;
         }
         const subCategory4 = getSubCategory(4);
         if (subCategory4) {
-            potentialAffetc  = subCategory4.explanation ? safeJsonParse(subCategory2.explanation) : undefined;
+            // potentialAffetc  = subCategory4.explanation ? safeJsonParse(subCategory2.explanation) : undefined;
+            potentialAffetc  = subCategory4.explanation ? subCategory4.explanation : undefined;
         }
         // const subCategory4 = getSubCategory(4);
         // if (subCategory4) {
@@ -80,17 +104,17 @@ const SubInstrument = () => {
         // }
         const subCategory5 = getSubCategory(5);
         if (subCategory5) {
-            const parsed = safeJsonParse(subCategory5.other);
-            beforeAndAfterImage = parsed ? parsed.beforeAndAfterImage : undefined;  
-          
+            // const parsed = safeJsonParse(subCategory5.other);
+            beforeAndAfterImage = subCategory5.other ? subCategory5.other : undefined;  
+            console.log('Before and After: ', beforeAndAfterImage);
         }
         const subCategory6 = getSubCategory(6);
         if (subCategory6) {
-            reference = subCategory6.other ? safeJsonParse(subCategory6.other) : undefined;
+            reference = subCategory6.other ? [subCategory6.other] : undefined;
         }
         const subCategory7 = getSubCategory(7);
         if (subCategory7) {
-            cardInfo=data.data.subcategories[7].other ? safeJsonParse(subCategory7.other) : undefined;
+            cardInfo = subCategory7.other ? safeJsonParse(subCategory7.other) : undefined;
         }
     }
 
@@ -195,7 +219,15 @@ const SubInstrument = () => {
             window.removeEventListener('scroll', handleScroll);
         }
     }, []);
-        
+
+
+    if (error) {
+        return (
+            <div className='instrument-error-page'>
+                <h1 className='instrument-error-title'>Unable to find this instrument. Please try again later.</h1>
+            </div>
+        )
+    }    
     return (
         <div className='outer-container'>
         <div className='sub-instrument-container'>
@@ -256,7 +288,7 @@ const SubInstrument = () => {
            <div className='sub-instrument-scroll-container' id = 'beforeAndAfter'>
               <SubTxt title={subcategories[5].subCategoryTitle} text={subcategories[5].explanation}/>
               {/* <SubProcedureScroll data={beforeAndAfterImage.beforeAndAfterImage} /> */}
-              <SubProcedureScroll data={beforeAndAfterImage} />
+              {/* <SubProcedureScroll data={beforeAndAfterImage} /> UNCOMMENT THIS AFTER WE ACTUALLY HAVE PICTURES! */}  
               <HomeLink title = "View More Post" href = '/posts'/>  
            </div>
             }
@@ -269,7 +301,7 @@ const SubInstrument = () => {
                <div className='sub-instrument-title'>
                    References and resources
                </div>
-               <SubProcedureReference reference = {reference} id = ''/>
+               <SubProcedureReference reference={reference} id = ''/>
            </div>
            }
            {isMobile && <SubProcedureMobileExtraBottom />  }
