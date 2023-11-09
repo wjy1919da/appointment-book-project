2e746936f35bd531fdc42e755cc5419024af4f48
Abhinav Mahesh <amahesh6@illinois.edu>
2023-11-08 18:40:21 -0600

    Buttons from tables redirect to Not Found Page

--- src/App.js
+++ src/App.js
@@ -33,7 +33,6 @@ import { Create } from '@mui/icons-material';
 import UserProfileSubArea from './components/user-profile-subArea/user-profile-subArea.jsx';
 
 import AccountSetup from './components/account-setting/account-setting';
-import AccountNotFoundPage from './components/account-setting/not-found';
 // import DoctorSignUpProcess from './components/doctor-signUp-process/doctor-signUp-process';
 
 const App = () => {
@@ -89,7 +88,6 @@ const App = () => {
         <Route path='userProfile' element={<UserProfilePage />} />
 
         <Route path='AccountSetup' element={<AccountSetup/>} />
-        <Route path='AccountNotFound' element={<AccountNotFoundPage/>} />
 
         {/* <Route path='register/verifyEmail/:token' element={<Verification />} /> */}
         <Route path='register/verifyEmail' element={<Verification />} />
--- src/components/account-setting/not-found.jsx
+++ src/components/account-setting/not-found.jsx
@@ -1,15 +1,28 @@
+import { useState } from 'react';
 import Footer from '../../components/footer/footer.component';
+import AccountSetup from './account-setting';
 import "./not-found.styles.scss";
 
 const AccountNotFoundPage = () => {
+    const [showAccountSetting, setShowAccountSetting] = useState(false);
+
+    const handleClick = () => {
+        setShowAccountSetting(true);
+    };
     return (
         <div className="not-found-page-container">
-            <div className="not-found-page-screen">
-                <button class="back-home-button"></button>
-                <span className='not-found-text'>404 Not Found</span>
-                <span className='feature-coming-soon-text'>Feature is Coming Soon!</span>
+            {showAccountSetting ? (
+                <AccountSetup/>
+            ) : (
+            <div>
+                <div className="not-found-page-screen">
+                    <button class="back-home-button" onClick={handleClick}></button>
+                    <span className='not-found-text'>404 Not Found</span>
+                    <span className='feature-coming-soon-text'>Feature is Coming Soon!</span>
+                </div>
+                <Footer/>
             </div>
-            <Footer/>
+            )}
         </div>
     )
 }
