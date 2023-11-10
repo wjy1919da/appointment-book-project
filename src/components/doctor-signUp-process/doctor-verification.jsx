7f47f700acb87c678a2983ea60162c1135f9e971
wjy1919da <wjy1917886@gmail.com>
2023-11-10 18:55:08 -0500

    add uploading and upload logic
    when file uploaded, it will go to uploaded section.

--- src/components/doctor-signUp-process/doctor-verification.jsx
+++ src/components/doctor-signUp-process/doctor-verification.jsx
@@ -5,28 +5,32 @@ import UploadIcon from '../../assets/doctor/Upload.png'
 import { Button, Dropdown, Form } from 'react-bootstrap';
 import {Upload} from "@aws-sdk/lib-storage";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import React from "react";
+// import S3 from "react-aws-s3";
 import { useState, useRef } from 'react';
 import userInfoQueryStore from '../../userStore';
-// import doctorInfoQueryStxore from '../../doctorStore.ts';
-import SignupAndLoginButton from '../components-signup-and-login/signup-and-login-button/signup-and-login-button.component';
-const s3Client = new S3Client({
-    region: 'us-west-1', // 例如 'us-west-2'
-    credentials: {
-        accessKeyId: 'AKIAWQE6ZUZGA6LCR3UX',
-        secretAccessKey: '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'
-    }
 
-  });
+const s3Client = new S3Client({
+    region: process.env.REACT_APP_REGION, // 例如 'us-west-2'
+    credentials: {
+        accessKeyId: process.env.REACT_APP_ACCESS_ID,
+        secretAccessKey:  process.env.REACT_APP_ACCESS_KEY
+    }
+});
+// const ReactS3Client = new S3(config);
 const DoctorVerification = () => {
+    console.log("process", process.env);
     const [selectedFiles, setSelectedFiles] = useState([]);
-    const switchPopupTab = userInfoQueryStore(state => state.switchPopupTab);
     const fileInputRef = useRef(null);
+    // 新状态来跟踪正在上传的文件和已上传的文件
+    const [uploadingFiles, setUploadingFiles] = useState([]);
+    const [uploadedFiles, setUploadedFiles] = useState([]);
     const uploadToS3 = async (file) => {
         const fileName = `${Date.now()}-${file.name}`;
         const upload = new Upload({
             client: s3Client,
             params: {
-                Bucket: 'verificationbucketcharm',
+                Bucket: process.env.REACT_APP_BUCKET_NAME,
                 Key: fileName,
                 Body: file,
             },
@@ -42,10 +46,45 @@ const DoctorVerification = () => {
         try {
             const data = await upload.done();
             console.log('上传成功', data);
+            setUploadingFiles(current => current.filter(f => f.name !== file.name));
+            setUploadedFiles(current => [...current, file]);
             // 文件上传成功后的操作
         } catch (err) {
             console.log('上传错误', err);
         }
+    };
+    const handleFileSelection = (event) => {
+        const newFiles = Array.from(event.target.files);
+        setSelectedFiles(prevFiles => {
+            const existingFileNames = new Set(prevFiles.map(file => file.name));
+            const newUniqueFiles = newFiles.filter(file => !existingFileNames.has(file.name));
+            return [...prevFiles, ...newUniqueFiles];
+        });
+    };    
+    
+    const handleUploadClick = async () => {
+        console.log(`正在上传 ${selectedFiles.length} 个文件...`);
+        const uploadPromises = selectedFiles.map(file => uploadToS3(file));
+        
+        try {
+            // 等待所有文件上传完成
+            const uploadResults = await Promise.all(uploadPromises);
+            console.log('所有文件上传成功', uploadResults);
+            setUploadingFiles(selectedFiles); // 将选择的文件设置为正在上传状态
+            setSelectedFiles([]); // 清空选择的文件列表
+        } catch (err) {
+            console.error('上传过程中出现错误', err);
+        }
+    };
+    const handleClickUploadBox = () => {
+        // Trigger the hidden file input click event
+        if (fileInputRef.current) {
+            fileInputRef.current.click();
+        }
+    };
+    const handleRemoveFile = (fileName) => {
+        // 过滤掉需要删除的文件
+        setSelectedFiles(prevFiles => prevFiles.filter(file => file.name !== fileName));
     };
     const handleBrowseFiles = () => {
         fileInputRef.current.click();
@@ -58,47 +97,6 @@ const DoctorVerification = () => {
         e.preventDefault();
         handleFileSelection({ target: { files: e.dataTransfer.files } });
     };
-
-    const handleFileSelection = (event) => {
-        // 获取新选中的所有文件并转换为数组
-        const newFilesArray = Array.from(event.target.files);
-    
-        // 更新状态以添加不重复的新文件
-        setSelectedFiles(prevFiles => {
-            // 创建一个包含所有当前文件名的集合
-            const existingFileNames = new Set(prevFiles.map(file => file.name));
-    
-            // 过滤掉已经存在的文件
-            const filteredNewFiles = newFilesArray.filter(file => !existingFileNames.has(file.name));
-    
-            // 将过滤后的新文件添加到已有文件数组中
-            return [...prevFiles, ...filteredNewFiles];
-        });
-    };    
-    const handleRemoveFile = (fileName) => {
-        // 过滤掉需要删除的文件
-        setSelectedFiles(prevFiles => prevFiles.filter(file => file.name !== fileName));
-    };
-    const handleUploadClick = async () => {
-        console.log(`正在上传 ${selectedFiles.length} 个文件...`);
-        const uploadPromises = selectedFiles.map(file => uploadToS3(file));
-        
-        try {
-            // 等待所有文件上传完成
-            const uploadResults = await Promise.all(uploadPromises);
-            console.log('所有文件上传成功', uploadResults);
-            // 可以在这里调用 switchPopupTab 或其他函数来处理上传成功后的逻辑
-        } catch (err) {
-            console.error('上传过程中出现错误', err);
-        }
-    };
-    const handleClickUploadBox = () => {
-        // Trigger the hidden file input click event
-        if (fileInputRef.current) {
-            fileInputRef.current.click();
-        }
-    };
-
     return (
         <div className='doctor-verification-main-container'>
             <div className='doctor-verification-title'>
@@ -132,47 +130,15 @@ const DoctorVerification = () => {
                     Step 1 of 1
                 </span>
                 <span className='doctor-verification-text'>
-                {selectedFiles.length === 0 
-                    ? "Please upload all required medical licenses or certifications to verify your profile." 
-                    : `${selectedFiles.length} file(s) selected`}
+                     Please upload all required medical licenses or certifications to verify your profile.
                 </span>
-                {selectedFiles.map((file, index) => (
-                    <div key={index} className="selected-file">
-                        <span>{file.name}</span>
-                        <button onClick={() => handleRemoveFile(file.name)}>delete</button>
-                    </div>
-                ))}
-                {/* <input type="file" onChange={handleFileSelection} multiple className='doctor-verification-text' /> */}
             </div>
             <div className='doctor-verification-upload-body'
-                    onDrop={handleDrop}
-                    onDragOver={handleDragOver}
-                    onClick={handleBrowseFiles}>
-                    <img src={UploadIcon} alt="Upload Icon" />
-                    <span className='doctor-verification-text'>Drag and Drop files or Browse</span>
-                </div>
-                <input
-                    type="file"
-                    ref={fileInputRef}
-                    onChange={handleFileSelection}
-                    multiple
-                    style={{ display: 'none' }}
-                />
-            <div className='doctor-verification-choose-file-section'>
-                <span className='doctor-verification-text'>Uploading 3/3 files</span>
-                <div className='uploadFile-box'>
-                    <span className='doctor-verification-text' style={{marginLeft:'10px'}}>license.PDF</span>
-                    <img src = {CloseButton} style={{width:'15px',height:'15px',marginRight:'10px'}}></img>
-                </div>
-            </div>
-            <div className='doctor-verification-list-file-section'>
-                <span className='doctor-verification-text'>Upload</span>
-                <div className='uploadFile-box' onClick={handleClickUploadBox}>
-                    <span className='doctor-verification-text' style={{marginLeft:'10px'}}>license.PDF</span>
-                </div>
-                <div className='uploadFile-box' onClick={handleClickUploadBox}>
-                    <span className='doctor-verification-text' style={{marginLeft:'10px'}}>license.PDF</span>
-                </div>
+                onDrop={handleDrop}
+                onDragOver={handleDragOver}
+                onClick={handleBrowseFiles}>
+                <img src={UploadIcon} alt="Upload Icon" />
+                <span className='doctor-verification-text'>Drag and Drop files or Browse</span>
             </div>
             <input
                 type="file"
@@ -181,6 +147,32 @@ const DoctorVerification = () => {
                 multiple
                 style={{ display: 'none' }}
             />
+            <div className='doctor-verification-choose-file-section'>
+                <span className='doctor-verification-text'>Uploading {selectedFiles.length}/3 files</span>
+                 {selectedFiles.map((file) => (
+                    <div key={file.name} className="selected-file">
+                        <div className='uploadFile-box'>
+                            <span className='doctor-verification-text' style={{ marginLeft: '10px' }}>{file.name}</span>
+                            <img 
+                                src={CloseButton}
+                                onClick={() => handleRemoveFile(file.name)}
+                                style={{ width: '15px', height: '15px', marginRight: '10px', cursor: 'pointer' }}
+                                alt="Remove"
+                            />
+                        </div>
+                    </div>
+                ))}
+            </div> 
+            <div className='doctor-verification-list-file-section'>
+                <span className='doctor-verification-text'>Upload</span>
+                {uploadedFiles.map((file) => (
+                    <div key={file.name} className="selected-file">
+                        <div className='uploadFile-box'>
+                            <span className='doctor-verification-text' style={{ marginLeft: '10px' }}>{file.name}</span>
+                        </div>
+                    </div>
+                ))}
+            </div>
             <div className='doctor-verification-upload-button'>
                 <button className="doctor-verification-upload-button" onClick={handleUploadClick}>UPLOAD FILES</button>
             </div>
