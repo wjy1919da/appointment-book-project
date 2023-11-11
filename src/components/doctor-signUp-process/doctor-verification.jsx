e2d48b4f39509ef22a7e2ac911e84771ef360be3
wjy1919da <wjy1917886@gmail.com>
2023-11-10 22:22:38 -0500

    save current status of upload file
    1. only able to load small files 2. the secret key and id store in front end is not safe

--- src/components/doctor-signUp-process/doctor-verification.jsx
+++ src/components/doctor-signUp-process/doctor-verification.jsx
@@ -6,20 +6,19 @@ import { Button, Dropdown, Form } from 'react-bootstrap';
 import {Upload} from "@aws-sdk/lib-storage";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import React from "react";
-// import S3 from "react-aws-s3";
 import { useState, useRef } from 'react';
-import userInfoQueryStore from '../../userStore';
 
 const s3Client = new S3Client({
-    region: process.env.REACT_APP_REGION, // 例如 'us-west-2'
+    region: process.env.REACT_APP_REGION,
     credentials: {
-        accessKeyId: process.env.REACT_APP_ACCESS_ID,
-        secretAccessKey:  process.env.REACT_APP_ACCESS_KEY
-    }
+      accessKeyId: process.env.REACT_APP_ACCESS_ID,
+      secretAccessKey: process.env.REACT_APP_ACCESS_KEY
+    },
+    logger: console // 这会输出日志到控制台
 });
-// const ReactS3Client = new S3(config);
+  
 const DoctorVerification = () => {
-    console.log("process", process.env);
+   // console.log("process", process.env);
     const [selectedFiles, setSelectedFiles] = useState([]);
     const fileInputRef = useRef(null);
     // 新状态来跟踪正在上传的文件和已上传的文件
@@ -30,28 +29,29 @@ const DoctorVerification = () => {
         const upload = new Upload({
             client: s3Client,
             params: {
-                Bucket: process.env.REACT_APP_BUCKET_NAME,
-                Key: fileName,
-                Body: file,
+              Bucket: process.env.REACT_APP_BUCKET_NAME,
+              Key: fileName,
+              Body: file,
             },
-            // 可以增加一些配置来控制上传的部分大小和并发性
-            partSize: 5 * 1024 * 1024, // 例如，设置每个部分大小为 5MB
+            partSize: 8 * 1024 * 1024, // 设置每个部分大小为 1MB
             queueSize: 4, // 同时上传的部分数量
-        });
-    
-        upload.on('httpUploadProgress', (progress) => {
-            console.log(`上传进度：${Math.round(progress.loaded / progress.total * 100)}%`);
-        });
-    
-        try {
+          });
+        
+          upload.on('httpUploadProgress', (progress) => {
+            console.log(`上传进度：${Math.round((progress.loaded / progress.total) * 100)}%`);
+          });
+        
+          try {
             const data = await upload.done();
-            console.log('上传成功', data);
-            setUploadingFiles(current => current.filter(f => f.name !== file.name));
-            setUploadedFiles(current => [...current, file]);
-            // 文件上传成功后的操作
-        } catch (err) {
-            console.log('上传错误', err);
-        }
+            if (data.$metadata.httpStatusCode === 200) {
+              // 所有分段都已成功上传
+              console.log('上传成功', data);
+              // 这里可以根据需要更新应用程序的状态或 UI
+            }
+          } catch (err) {
+            console.error('上传错误', err);
+            // 这里可以处理上传失败的情况
+          }
     };
     const handleFileSelection = (event) => {
         const newFiles = Array.from(event.target.files);
@@ -69,17 +69,11 @@ const DoctorVerification = () => {
         try {
             // 等待所有文件上传完成
             const uploadResults = await Promise.all(uploadPromises);
-            console.log('所有文件上传成功', uploadResults);
+            console.log('所有文件上传结束', uploadResults);
             setUploadingFiles(selectedFiles); // 将选择的文件设置为正在上传状态
             setSelectedFiles([]); // 清空选择的文件列表
         } catch (err) {
             console.error('上传过程中出现错误', err);
-        }
-    };
-    const handleClickUploadBox = () => {
-        // Trigger the hidden file input click event
-        if (fileInputRef.current) {
-            fileInputRef.current.click();
         }
     };
     const handleRemoveFile = (fileName) => {
