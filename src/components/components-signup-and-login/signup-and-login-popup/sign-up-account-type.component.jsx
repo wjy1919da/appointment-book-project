b1861ab698c6a5567d2218d937a3732d09ef6740
wjy1919da <wjy1917886@gmail.com>
2023-11-12 14:45:17 -0500

    fix: instrument, login form, accountType
    fix instrument page merging issue, delete close button in login form, update accountType type
    pending: side bar in procedure page and instrument page

--- src/components/components-signup-and-login/signup-and-login-popup/sign-up-account-type.component.jsx
+++ src/components/components-signup-and-login/signup-and-login-popup/sign-up-account-type.component.jsx
@@ -4,9 +4,10 @@ import './sign-up-account-type.styles.scss';
 import userInfoQueryStore from '../../../userStore.ts';
 import NextButton from './next-button.component'; 
 import MemberAndDoctorButton from './member-doctor-button.component'; 
-import { useEffect } from 'react';
+import { useEffect,useState } from 'react';
 
 const SignUpAccountType = () => {
+    const [activeButtonType, setActiveButtonType] = useState(null);
     const setAccountType = userInfoQueryStore((state) => state.setAccountType);
     const switchPopupTab = userInfoQueryStore(state => state.switchPopupTab);
     const userInfo = userInfoQueryStore((state) => state.userInfo);
@@ -27,32 +28,26 @@ const SignUpAccountType = () => {
                     <MemberAndDoctorButton 
                         title="Doctor"
                         onClick={() => {
-                            console.log('Doctor button clicked');  
+                            //console.log('Doctor button clicked');  
                             //setAccountType(0);
-                            if (userInfo.accountType === 2) {
-                                localStorage.setItem('accountType', null);
-                                setAccountType(null);
-                            } else {
-                                localStorage.setItem('accountType', 2);
-                                setAccountType(2);
-                            }
+                            let newType = userInfo.accountType === 2 ? null : 2;
+                            localStorage.setItem('accountType', newType);
+                            setAccountType(newType);
+                            setActiveButtonType(newType === 2 ? 'doctor' : null);
                         }}
-                        disabled={userInfo.accountType && userInfo.accountType !== 2}
+                        isActive={activeButtonType === 'doctor'}
                     />
                     <MemberAndDoctorButton 
                         title="Member"
                         onClick={() => {
-                            console.log('Member button clicked');  
+                           // console.log('Member button clicked');  
                             //setAccountType(0);
-                            if (userInfo.accountType === 1) {
-                                localStorage.setItem('accountType', null);
-                                setAccountType(null);
-                            } else {
-                                localStorage.setItem('accountType', 1);
-                                setAccountType(1);
-                            }
+                            let newType = userInfo.accountType === 1 ? null : 1;
+                            localStorage.setItem('accountType', newType);
+                            setAccountType(newType);
+                            setActiveButtonType(newType === 1 ? 'member' : null);
                         }}
-                        disabled={userInfo.accountType && userInfo.accountType !== 1}
+                        isActive={activeButtonType === 'member'}
                     />
                 </div>
             </div>
