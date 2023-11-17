9666fda94e377a51f978f4684f33d90523d8c6c5
Emmy Manning <eobcre@gmail.com>
2023-11-17 13:44:03 -0500

    updated main doctor page

--- src/routes/individual-doctor/individual-doctor.jsx
+++ src/routes/individual-doctor/individual-doctor.jsx
@@ -1,19 +1,26 @@
 import React, { useEffect, useLayoutEffect, useState } from 'react';
-import './individual-doctor.styles.scss';
+import { useParams } from 'react-router-dom';
+import useDoctorQueryStore from '../../store.ts';
+
+// components
 import DoctorProfile from '../../components/component-individual-doctor/doctor-profile/doctor-profile';
 import DoctorAbout from '../../components/component-individual-doctor/doctor-about/doctor-about.component';
 import HomeSpinner from '../../components/home-spinner/home-spinner.component';
-import { useParams } from 'react-router-dom';
-import useDoctorQueryStore from '../../store.ts';
-import UserProfileDoctorPostGrid from '../../components/user-profile-doctor-post-grid/user-profile-doctor-post-grid';
-import { useGetDoctorInfo } from '../../hooks/useGetIndividualDoctor.js';
 import DoctorReviewGrid from '../../components/component-individual-doctor/doctor-review-grid/doctor-review-grid.component';
+import UserProfileDoctorPostGrid from '../../components/user-profile-doctor-post-grid/user-profile-doctor-post-grid';
 import ErrorMsg from '../../components/error-msg/error-msg.component';
+
+// hook
+import { useGetDoctorInfo } from '../../hooks/useGetIndividualDoctor.js';
+
+// scss
+import './individual-doctor.styles.scss';
 
 const IndividualDoctor = () => {
   useLayoutEffect(() => {
     window.scrollTo(0, 0);
   });
+
   const { encodedMemberId } = useParams();
   const doctorQuery = useDoctorQueryStore((state) => state.doctorQuery);
   const setMemberId = useDoctorQueryStore((state) => state.setMemberId);
@@ -22,63 +29,30 @@ const IndividualDoctor = () => {
   const { nickname } = data?.nickname || {};
   const [activeTab, setActiveTab] = useState(0);
   const tabs = ['About', 'Posts', 'Reviews'];
+
   useEffect(() => {
     setMemberId(encodedMemberId);
     if (data) {
       setNickName(data.nickname);
     }
   }, [encodedMemberId, data]);
+
   if (isLoading) {
     return <HomeSpinner />;
   }
+
   if (error) {
     return (
-      <div className='container'>
-        <div className='individual-doctor-container'>
-          <div className='individual-doctor-left-container'>
-            {data && (
-              <DoctorProfile
-                nickname={data.nickname}
-                projects={data.name}
-                mechName={data.mechName}
-                address={data.address}
-              />
-            )}
-          </div>
-          <div className='individual-doctor-right-container'>
-            <div className='individual-doctor-tabs'>
-              {tabs.map((item, index) => (
-                <div
-                  key={index}
-                  className={`individual-doctor-tab ${
-                    activeTab === index ? 'active' : ''
-                  }`}
-                  onClick={() => selectTab(index)}
-                >
-                  {item}
-                  <div className='individual-doctor-tab-underline'></div>
-                </div>
-              ))}
-            </div>
-            {activeTab === 0 && (
-              <div className='individual-doctor-right-about'>
-                <DoctorAbout />
-              </div>
-            )}
-            {activeTab === 1 && (
-              <div className='individual-doctor-posts'>
-                <DoctorPostGrid isAbout={true} />
-              </div>
-            )}
-            {activeTab === 2 && <DoctorReviewGrid />}
-          </div>
-        </div>
+      <div>
+        <ErrorMsg />
       </div>
     );
   }
+
   const selectTab = (index) => {
     setActiveTab(index);
   };
+
   return (
     <div className='individual-page-container'>
       <div className='individual-doctor-container'>
