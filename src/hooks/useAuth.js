3a657e61e9b2b458be6adf796924af009a685173
wjy1919da <wjy1917886@gmail.com>
2023-11-19 20:24:39 -0500

    Fix: individual doctor page should be in center
    update

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,139 +1,210 @@
-import userInfoQueryStore from '../userStore.ts';
-import { useMutation } from 'react-query';
+import userInfoQueryStore from "../userStore.ts";
+import { useMutation } from "react-query";
 import { useQuery } from "react-query";
-import APIClient from '../services/api-client.js';
+import APIClient from "../services/api-client.js";
 
 export function useUserOptLogin() {
-    const apiClient = new APIClient('/login/phone/validate-otp');
-    const fetchUserOtpRegisterValidate = async (mobile,otp,userRole) => {
-        const res = await apiClient.post({
-            mobile,
-            otp,
-            userRole
-        });
-        return res.data;
-    };
-    return useMutation((credentials) => fetchUserOtpRegisterValidate(credentials.mobile, credentials.otp, credentials.userRole));
+  const apiClient = new APIClient("/login/phone/validate-otp");
+  const fetchUserOtpRegisterValidate = async (mobile, otp, userRole) => {
+    const res = await apiClient.post({
+      mobile,
+      otp,
+      userRole,
+    });
+    return res.data;
+  };
+  return useMutation((credentials) =>
+    fetchUserOtpRegisterValidate(
+      credentials.mobile,
+      credentials.otp,
+      credentials.userRole
+    )
+  );
 }
 
 export function useUserRegister() {
-    const apiClient = new APIClient('/register/email');
-    const fetchUserRegister = async (email,password,userRole) => {
-        const res = await apiClient.post({
-            email,
-            password,
-            userRole
-        });
-        return res.data;
-    };
-    return useMutation((credentials) => fetchUserRegister(credentials.email, credentials.password, credentials.userRole));
+  const apiClient = new APIClient("/register/email");
+  const fetchUserRegister = async (email, password, userRole) => {
+    const res = await apiClient.post({
+      email,
+      password,
+      userRole,
+    });
+    return res.data;
+  };
+  return useMutation((credentials) =>
+    fetchUserRegister(
+      credentials.email,
+      credentials.password,
+      credentials.userRole
+    )
+  );
 }
 
-export function useUserEmailLogin(){
-    const apiClient = new APIClient('/login/user');
-    const fetchEmailLogin = async (email, password,provider,userRole) => {
-        return apiClient.post({
-            email,
-            password,
-            provider,
-            userRole
-        }).then(res => res.data);
-    };
-    return useMutation((credentials) => fetchEmailLogin(credentials.email, credentials.password, credentials.provider, credentials.userRole));
+export function useUserEmailLogin() {
+  const apiClient = new APIClient("/login/user");
+  const fetchEmailLogin = async (email, password, provider, userRole) => {
+    return apiClient
+      .post({
+        email,
+        password,
+        provider,
+        userRole,
+      })
+      .then((res) => res.data);
+  };
+  return useMutation((credentials) =>
+    fetchEmailLogin(
+      credentials.email,
+      credentials.password,
+      credentials.provider,
+      credentials.userRole
+    )
+  );
 }
 export function useSocialLogin() {
-    const apiClient = new APIClient('/login/social');
-    const fetchSocialLogin = async (googleAccessToken, provider) => {
-        const res = await apiClient.post({
-            googleAccessToken,
-            provider
-        });
-        return res.data;
-    };
-    return useMutation((credentials) => fetchSocialLogin(credentials.googleAccessToken, credentials.provider));
+  const apiClient = new APIClient("/login/social");
+  const fetchSocialLogin = async (googleAccessToken, provider) => {
+    const res = await apiClient.post({
+      googleAccessToken,
+      provider,
+    });
+    return res.data;
+  };
+  return useMutation((credentials) =>
+    fetchSocialLogin(credentials.googleAccessToken, credentials.provider)
+  );
 }
 
-
-export function useUserOtpRegister(){
-    const apiClient = new APIClient('/register/phone/send-otp');
-    const fetchUserOtpRegister = async (phoneNumber) => {
-        const res = await apiClient.post( {
-            "mobile": phoneNumber,
-        });
-        return res.data;
-    };
-    return useMutation((credentials) => fetchUserOtpRegister(credentials.phoneNumber));
+export function useUserOtpRegister() {
+  const apiClient = new APIClient("/register/phone/send-otp");
+  const fetchUserOtpRegister = async (phoneNumber) => {
+    const res = await apiClient.post({
+      mobile: phoneNumber,
+    });
+    return res.data;
+  };
+  return useMutation((credentials) =>
+    fetchUserOtpRegister(credentials.phoneNumber)
+  );
 }
-export function useUserOtpRegisterValidate(){
-    const apiClient = new APIClient('/register/phone/validate-otp');
-    const fetchUserOtpRegisterValidate = async (mobile,otp,userRole) => {
-        const res = await apiClient.post({
-            mobile,
-            otp,
-            userRole
-        });
-        return res.data;
-    };
-    return useMutation((credentials) => fetchUserOtpRegisterValidate(credentials.mobile, credentials.otp, credentials.userRole));
+export function useUserOtpRegisterValidate() {
+  const apiClient = new APIClient("/register/phone/validate-otp");
+  const fetchUserOtpRegisterValidate = async (mobile, otp, userRole) => {
+    const res = await apiClient.post({
+      mobile,
+      otp,
+      userRole,
+    });
+    return res.data;
+  };
+  return useMutation((credentials) =>
+    fetchUserOtpRegisterValidate(
+      credentials.mobile,
+      credentials.otp,
+      credentials.userRole
+    )
+  );
 }
 export function useUserEmailRegisterValidate(token) {
-    const apiClient = new APIClient('/register/verifyEmail');
-    const fetchUserEmailRegisterValidate = async ({queryKey}) => {
-        const [, token] = queryKey;
-        const res = await apiClient.get({ token });
-        return res.data;
-    }
-    return useQuery(['userEmailRegisterValidate', token], (token)=>fetchUserEmailRegisterValidate(token));
+  const apiClient = new APIClient("/register/verifyEmail");
+  const fetchUserEmailRegisterValidate = async ({ queryKey }) => {
+    const [, token] = queryKey;
+    const res = await apiClient.get({ token });
+    return res.data;
+  };
+  return useQuery(["userEmailRegisterValidate", token], (token) =>
+    fetchUserEmailRegisterValidate(token)
+  );
 }
-export function useSetUserProfile(){
-    const apiClient = new APIClient('/user/set_user_profile');
-    //const token = localStorage.getItem('token');
-    const fetchSetUserProfile = async (gender, interestArea, email,birthday,nickname) => {
-            // if (!token) {
-            //     alert('user not login');
-            // }
-            const res = await apiClient.post({
-                gender,
-                interestArea,
-                email,
-                birthday,
-                nickname
-            },
-        );
-        return res.data;
-    };
-    return useMutation((credentials) => fetchSetUserProfile(credentials.gender, credentials.interestArea, credentials.email, credentials.birthday,credentials.nickname));
+export function useSetUserProfile() {
+  const apiClient = new APIClient("/user/set_user_profile");
+  //const token = localStorage.getItem('token');
+  const fetchSetUserProfile = async (
+    gender,
+    interestArea,
+    email,
+    birthday,
+    nickname
+  ) => {
+    // if (!token) {
+    //     alert('user not login');
+    // }
+    const res = await apiClient.post({
+      gender,
+      interestArea,
+      email,
+      birthday,
+      nickname,
+    });
+    return res.data;
+  };
+  return useMutation((credentials) =>
+    fetchSetUserProfile(
+      credentials.gender,
+      credentials.interestArea,
+      credentials.email,
+      credentials.birthday,
+      credentials.nickname
+    )
+  );
 }
-export function useClickVerification(){
-    const apiClient = new APIClient('/register/clickVerification');
-    const fetchClickVerification = async (email, userRole) => {
-        const res = await apiClient.post({
-            email,
-            userRole
-        });
-        return res.data;
-    };
-    return useMutation((credentials) => fetchClickVerification(credentials.email, credentials.userRole));
+export function useClickVerification() {
+  const apiClient = new APIClient("/register/clickVerification");
+  const fetchClickVerification = async (email, userRole) => {
+    const res = await apiClient.post({
+      email,
+      userRole,
+    });
+    return res.data;
+  };
+  return useMutation((credentials) =>
+    fetchClickVerification(credentials.email, credentials.userRole)
+  );
 }
-export function useDoctorLogin(){
-    const apiClient = new APIClient('/login/doctor');
-    const fetchDoctorLogin = async (email, password,provider,userRole) => {
-        const res = await apiClient.post({
-            email,
-            password,
-            provider,
-            userRole
-        });
-        return res.data;
-    };
-    return useMutation((credentials) => fetchDoctorLogin(credentials.email, credentials.password, credentials.provider, credentials.userRole));
+export function useDoctorLogin() {
+  const apiClient = new APIClient("/login/doctor");
+  const fetchDoctorLogin = async (email, password, provider, userRole) => {
+    const res = await apiClient.post({
+      email,
+      password,
+      provider,
+      userRole,
+    });
+    return res.data;
+  };
+  return useMutation((credentials) =>
+    fetchDoctorLogin(
+      credentials.email,
+      credentials.password,
+      credentials.provider,
+      credentials.userRole
+    )
+  );
 }
-export function useGetUserInfo(){  
-    const apiClient = new APIClient('/user/fetch_user_profile');
-    const userInfo = userInfoQueryStore(s => s.userInfo);
-    const fetchGetUserInfo = async () => {
-        const res = await apiClient.post();
-        return res.data;
-    };
-    return useQuery(['getUserInfo', userInfo.token], fetchGetUserInfo);
+export function useGetUserInfo() {
+  const apiClient = new APIClient("/user/fetch_user_profile");
+  const userInfo = userInfoQueryStore((s) => s.userInfo);
+  const fetchGetUserInfo = async () => {
+    const res = await apiClient.post();
+    return res.data;
+  };
+  return useQuery(
+    ["getUserInfo", userInfo.token, userInfo.profile],
+    fetchGetUserInfo
+  );
+}
+export function useGetDoctorInfo() {
+  const userInfo = userInfoQueryStore((s) => s.userInfo);
+  const apiClient = new APIClient("/user_action/doctor_profile");
+
+  const fetchGetDoctorInfo = async () => {
+    const res = await apiClient.get(userInfo.userId);
+    return res.data;
+  };
+
+  return useQuery(
+    ["getDoctorInfo", userInfo.token, userInfo.profile],
+    fetchGetDoctorInfo
+  );
 }
--- src/userStore.ts
+++ src/userStore.ts
@@ -1,9 +1,9 @@
-import {create} from "zustand";
+import { create } from "zustand";
 import jwt_decode from "jwt-decode";
 
 interface DecodedToken {
-    sub: string;
-    [key: string]: any;
+  sub: string;
+  [key: string]: any;
 }
 /*
     1. open popup
@@ -18,153 +18,228 @@ interface DecodedToken {
 */
 
 interface userInfo {
-    token?: string;
-    googleToken?: string;
-    userId?: string;
-    email?: string;
-    phoneNumber?: string;
-    gender?: number| null;
-    accountType: number | null;
-    birthday?: string| null;
-    username?: string;
-    password?: string;
-    selectedInterests: Set<string>| null;
-     // Used to control the register popup page and open/close
-    popupState: 'closed'| 'signUp' | 'gender' | 'interest' | 'birthday' | 'success' | 'verifyEmail' | 'login'| 'sendVerifyEmail'| 'phoneNumberLogin'| 'doctorProfile' |'doctorFinish'|'doctorSuccess';
+  token?: string;
+  googleToken?: string;
+  userId?: string;
+  email?: string;
+  phoneNumber?: string;
+  gender?: number | null;
+  accountType: number | null;
+  birthday?: string | null;
+  username?: string;
+  password?: string;
+  selectedInterests: Set<string> | null;
+  profile?: string;
+  // Used to control the register popup page and open/close
+  popupState:
+    | "closed"
+    | "signUp"
+    | "gender"
+    | "interest"
+    | "birthday"
+    | "success"
+    | "verifyEmail"
+    | "login"
+    | "sendVerifyEmail"
+    | "phoneNumberLogin"
+    | "doctorProfile"
+    | "doctorFinish"
+    | "doctorSuccess";
 }
 
 interface userInfoQuery {
-    userInfo: userInfo;
-    setEmail: (email: string) => void;
-    setPhoneNumber: (phoneNumber: string) => void;
-    setPassword: (password: string) => void;
-    setGender: (gender: number) => void;
-    setAccountType: (accountType: number) => void;
-    setInterested: (interest: string) => void;
-    setGoogleToken: (token: string) => void;
-    setUsername: (username: string) => void;
-    setBirthday: (birthday: string) => void;
-    setToken: (token: string) => void;
-    setUserId: (userId: string) => void;
-    removeToken: () => void;
-    togglePopup: (open: boolean, initialState?: 'gender' | 'interest' | 'birthday' | 'success' | 'verifyEmail' | 'login'| 'sendVerifyEmail'| 'phoneNumberLogin'| 'doctorProfile' |'doctorFinish'|'doctorSuccess') =>  void;
-    switchPopupTab: (tab: 'gender' | 'interest' | 'birthday' | 'success' | 'verifyEmail' | 'login'| 'sendVerifyEmail'| 'phoneNumberLogin'| 'doctorProfile' |'doctorFinish'|'doctorSuccess') => void;
+  userInfo: userInfo;
+  setEmail: (email: string) => void;
+  setPhoneNumber: (phoneNumber: string) => void;
+  setPassword: (password: string) => void;
+  setGender: (gender: number) => void;
+  setAccountType: (accountType: number) => void;
+  setInterested: (interest: string) => void;
+  setGoogleToken: (token: string) => void;
+  setUsername: (username: string) => void;
+  setBirthday: (birthday: string) => void;
+  setToken: (token: string) => void;
+  setUserId: (userId: string) => void;
+  removeToken: () => void;
+  setProfile: (profile: string) => void;
+  togglePopup: (
+    open: boolean,
+    initialState?:
+      | "gender"
+      | "interest"
+      | "birthday"
+      | "success"
+      | "verifyEmail"
+      | "login"
+      | "sendVerifyEmail"
+      | "phoneNumberLogin"
+      | "doctorProfile"
+      | "doctorFinish"
+      | "doctorSuccess"
+  ) => void;
+  switchPopupTab: (
+    tab:
+      | "gender"
+      | "interest"
+      | "birthday"
+      | "success"
+      | "verifyEmail"
+      | "login"
+      | "sendVerifyEmail"
+      | "phoneNumberLogin"
+      | "doctorProfile"
+      | "doctorFinish"
+      | "doctorSuccess"
+  ) => void;
 }
 
 const userInfoQueryStore = create<userInfoQuery>((set) => ({
-    userInfo: {
-        token: "",
-        userId: "",
-        email: "",
-        phoneNumber: "",
-        password: "",
-        gender: 0,
-        accountType: null,
-        birthday: "",
-        googleToken: "",
-        selectedInterests: new Set<string>(),
-        // Initial state is closed
-        popupState: "closed"
-    },
+  userInfo: {
+    token: "",
+    userId: "",
+    email: "",
+    phoneNumber: "",
+    password: "",
+    gender: 0,
+    accountType: null,
+    birthday: "",
+    googleToken: "",
+    selectedInterests: new Set<string>(),
+    profile: "",
+    // Initial state is closed
+    popupState: "closed",
+  },
 
-    setEmail: (email: string) => {
-        set((store) => ({
-            userInfo: { ...store.userInfo, email }
-        }));
-    },
-    setPhoneNumber: (phoneNumber: string) => {
-        set((store) => ({
-            userInfo: { ...store.userInfo, phoneNumber }
-        }));
-    },
-    setPassword: (password: string) => {
-        set((store) => ({
-            userInfo: { ...store.userInfo, password }
-        }));
-    },
-    setGender: (gender: number) => {
-        set((store) => ({
-            userInfo: { ...store.userInfo, gender }
-        }));
-    },
-    setAccountType: (accountType: number) => {
-        set((store) => ({
-            userInfo: { ...store.userInfo, accountType }
-        }));
-    },
-    setGoogleToken: (token: string) => {
-        set((store) => ({
-            userInfo: { ...store.userInfo, googleToken: token }
-        }));
-    },
-    setUsername: (username: string) => {
-        set((store) => ({
-            userInfo: { ...store.userInfo, username }
-        }));
-    },
-    setBirthday: (birthday: string) => {
-        set((store) => ({
-            userInfo: { ...store.userInfo, birthday }
-        }));
-    },
-    togglePopup: (open: boolean, initialState?: 'gender' | 'interest' | 'birthday' | 'success' | 'verifyEmail' | 'login'| 'sendVerifyEmail'| 'phoneNumberLogin'| 'doctorProfile' |'doctorFinish'|'doctorSuccess') => {
-        //console.log("togglePopup", open, initialState);
-        if (open) {
-            set((store) => ({
-                userInfo: { ...store.userInfo, popupState: initialState || 'gender' }
-            }));
-        } else {
-            set((store) => ({
-                userInfo: { ...store.userInfo, popupState: 'closed' }
-            }));
-        }
-    },
-    switchPopupTab: (tab: 'gender' | 'interest' | 'birthday' | 'success' | 'verifyEmail' | 'login'| 'sendVerifyEmail'| 'phoneNumberLogin'| 'doctorProfile' |'doctorFinish'|'doctorSuccess') => {
-        set((store) => ({
-            userInfo: { ...store.userInfo, popupState: tab }
-        }));
-    },
-    setInterested: (interest: string) => {
-        set((store) => {
-            const newInterests = new Set(store.userInfo.selectedInterests);
-            if (newInterests.has(interest)) {
-                newInterests.delete(interest);
-            } else {
-                newInterests.add(interest);
-            }
-            return {
-                userInfo: { 
-                    ...store.userInfo, 
-                    selectedInterests: newInterests
-                }
-            };
-        });
-    },
-    setUserId: (userId: string) => {
-        set((store) => ({
-            userInfo: { ...store.userInfo, userId }
-        }));
-    },
-    setToken: (token: string) => {
-        try {
-            const decodedToken: DecodedToken = jwt_decode(token);
-            const userId = decodedToken.sub;
-            set((store) => ({
-                userInfo: { ...store.userInfo, token, userId }
-            }));
-        } catch (error) {
-            console.error("Failed to decode the token", error);
-        }
-    },
-    removeToken: () => {
-        try {
-            set((store) => ({
-                userInfo: { ...store.userInfo, token: "", userId: "" }
-            }));
-        } catch (error) {
-            console.error("Failed to remove the token", error);
-        }
+  setEmail: (email: string) => {
+    set((store) => ({
+      userInfo: { ...store.userInfo, email },
+    }));
+  },
+  setPhoneNumber: (phoneNumber: string) => {
+    set((store) => ({
+      userInfo: { ...store.userInfo, phoneNumber },
+    }));
+  },
+  setPassword: (password: string) => {
+    set((store) => ({
+      userInfo: { ...store.userInfo, password },
+    }));
+  },
+  setGender: (gender: number) => {
+    set((store) => ({
+      userInfo: { ...store.userInfo, gender },
+    }));
+  },
+  setAccountType: (accountType: number) => {
+    set((store) => ({
+      userInfo: { ...store.userInfo, accountType },
+    }));
+  },
+  setGoogleToken: (token: string) => {
+    set((store) => ({
+      userInfo: { ...store.userInfo, googleToken: token },
+    }));
+  },
+  setUsername: (username: string) => {
+    set((store) => ({
+      userInfo: { ...store.userInfo, username },
+    }));
+  },
+  setBirthday: (birthday: string) => {
+    set((store) => ({
+      userInfo: { ...store.userInfo, birthday },
+    }));
+  },
+  setProfile: (profile: string) => {
+    set((store) => ({
+      userInfo: { ...store.userInfo, profile },
+    }));
+  },
+
+  togglePopup: (
+    open: boolean,
+    initialState?:
+      | "gender"
+      | "interest"
+      | "birthday"
+      | "success"
+      | "verifyEmail"
+      | "login"
+      | "sendVerifyEmail"
+      | "phoneNumberLogin"
+      | "doctorProfile"
+      | "doctorFinish"
+      | "doctorSuccess"
+  ) => {
+    //console.log("togglePopup", open, initialState);
+    if (open) {
+      set((store) => ({
+        userInfo: { ...store.userInfo, popupState: initialState || "gender" },
+      }));
+    } else {
+      set((store) => ({
+        userInfo: { ...store.userInfo, popupState: "closed" },
+      }));
     }
+  },
+  switchPopupTab: (
+    tab:
+      | "gender"
+      | "interest"
+      | "birthday"
+      | "success"
+      | "verifyEmail"
+      | "login"
+      | "sendVerifyEmail"
+      | "phoneNumberLogin"
+      | "doctorProfile"
+      | "doctorFinish"
+      | "doctorSuccess"
+  ) => {
+    set((store) => ({
+      userInfo: { ...store.userInfo, popupState: tab },
+    }));
+  },
+  setInterested: (interest: string) => {
+    set((store) => {
+      const newInterests = new Set(store.userInfo.selectedInterests);
+      if (newInterests.has(interest)) {
+        newInterests.delete(interest);
+      } else {
+        newInterests.add(interest);
+      }
+      return {
+        userInfo: {
+          ...store.userInfo,
+          selectedInterests: newInterests,
+        },
+      };
+    });
+  },
+  setUserId: (userId: string) => {
+    set((store) => ({
+      userInfo: { ...store.userInfo, userId },
+    }));
+  },
+  setToken: (token: string) => {
+    try {
+      const decodedToken: DecodedToken = jwt_decode(token);
+      const userId = decodedToken.sub;
+      set((store) => ({
+        userInfo: { ...store.userInfo, token, userId },
+      }));
+    } catch (error) {
+      console.error("Failed to decode the token", error);
+    }
+  },
+  removeToken: () => {
+    try {
+      set((store) => ({
+        userInfo: { ...store.userInfo, token: "", userId: "" },
+      }));
+    } catch (error) {
+      console.error("Failed to remove the token", error);
+    }
+  },
 }));
 export default userInfoQueryStore;
-
