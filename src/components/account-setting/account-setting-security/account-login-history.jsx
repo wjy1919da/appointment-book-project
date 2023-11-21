8afdd661846a9f7ec0c61f56b5d96cc9ceaa4b8c
Emmy Manning <eobcre@gmail.com>
2023-11-21 02:15:03 -0500

    updated login history form

--- src/components/account-setting/account-setting-security/account-login-history.jsx
+++ src/components/account-setting/account-setting-security/account-login-history.jsx
@@ -16,13 +16,41 @@ const LoginHistory = () => {
       <BackButton backButtonName='Account Setting' />
       <div className='account-setting-login-history-right-container'>
         <form className='account-setting-login-history-form'>
-          <h2 className='account-setting-login-history-title'>
-            Login History
-          </h2>
-          <div className='account-setting-login-history-list'>
+          <h2 className='account-setting-login-history-title'>Login History</h2>
+          <div className='account-setting-login-history-list-title'>
             <p>Device</p>
             <p>IP Address</p>
             <p>Login Time</p>
+          </div>
+
+          <div className='account-setting-login-hitosry-list'>
+            <p>Web</p>
+            <p>10.23.23.213</p>
+            <p>10 minutes ago</p>
+          </div>
+
+          <div className='account-setting-login-hitosry-list'>
+            <p>Web</p>
+            <p>10.23.23.213</p>
+            <p>10 minutes ago</p>
+          </div>
+
+          <div className='account-setting-login-hitosry-list'>
+            <p>Web</p>
+            <p>10.23.23.213</p>
+            <p>10 minutes ago</p>
+          </div>
+
+          <div className='account-setting-login-hitosry-list'>
+            <p>Web</p>
+            <p>10.23.23.213</p>
+            <p>10 minutes ago</p>
+          </div>
+
+          <div className='account-setting-login-hitosry-list'>
+            <p>Web</p>
+            <p>10.23.23.213</p>
+            <p>10 minutes ago</p>
           </div>
         </form>
       </div>
