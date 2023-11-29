1957ac30edb90252edcc3a730b2b8f170fb4fb39
Emmy Manning <eobcre@gmail.com>
2023-11-28 19:41:40 -0500

    refactor code

--- src/components/components-posts/community-post-create-page/community-post-create-page.jsx
+++ src/components/components-posts/community-post-create-page/community-post-create-page.jsx
@@ -1,34 +1,33 @@
-import React, { useState, useEffect, useRef } from "react";
-import { useForm } from "react-hook-form";
-import { useNavigate } from "react-router-dom";
-
-// scss
-import "./community-post-create-page.scss";
+import React, { useState, useRef } from 'react';
+import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
+import userInfoQueryStore from '../../../userStore';
+// import { useDisclosure } from "@chakra-ui/react";
 
 // components
-import FormButton from "../../components-posts/community-post-button/community-post-button";
-
-import PostDropDownFilter from "../community-post-dropdown-filter/community-post-dropdown-filter";
+import FormButton from '../../components-posts/community-post-button/community-post-button';
+import PostDropDownFilter from '../community-post-dropdown-filter/community-post-dropdown-filter';
 
 // hook
-import { useApiRequestPost } from "../../../hooks/useApiRequestPost";
-// import { useAddPost } from '../../../hooks/useAddingPost';
+import { useApiRequestPost } from '../../../hooks/useApiRequestPost';
+
+// scss
+import './community-post-create-page.scss';
 
 // images
-import createPostIcon from "../../../assets/post/create-post-icon.png";
-import Arrow from "../../../assets/post/iconoir_arrow-right.svg";
-import { uploadToS3 } from "../../../services/s3-client";
+import createPostIcon from '../../../assets/post/create-post-icon.png';
+import Arrow from '../../../assets/post/iconoir_arrow-right.svg';
+import { uploadToS3 } from '../../../services/s3-client';
 import {
   Alert,
   AlertIcon,
   AlertTitle,
   AlertDescription,
   CloseButton,
-} from "@chakra-ui/react";
-import { useDisclosure } from "@chakra-ui/react";
-import userInfoQueryStore from "../../../userStore";
+} from '@chakra-ui/react';
+
 const CreatePostPage = () => {
-  const [alert, setAlert] = useState({ show: false, type: "", message: "" });
+  const [alert, setAlert] = useState({ show: false, type: '', message: '' });
   const [clickedRadio, setClickedRadio] = useState(false);
   const [selectedFiles, setSelectedFiles] = useState([]);
   const fileInputRef = useRef(null);
@@ -36,39 +35,40 @@ const CreatePostPage = () => {
   const [uploadedFiles, setUploadedFiles] = useState([]);
   const userInfo = userInfoQueryStore((state) => state.userInfo);
   const navigate = useNavigate();
+
   // react hook form
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm({ mode: "onChange" });
+  } = useForm({ mode: 'onChange' });
 
   // api
   const { mutate: apiMutate } = useApiRequestPost({
     onError: (error) => {
       setAlert({
         show: true,
-        type: "error",
-        message: "Failed to create post.",
+        type: 'error',
+        message: 'Failed to create post.',
       });
     },
   });
 
   const onSubmit = (data) => {
     const formData = {
-      address: "",
+      address: '',
       brief: data.description,
-      coverImg: "",
+      coverImg: '',
       id: 0,
       isDisplay: 0,
-      lat: "",
-      location: "",
-      lon: "",
+      lat: '',
+      location: '',
+      lon: '',
       pictures: uploadedFiles,
       tags: [
         {
           tagId: 0,
-          tagName: "",
+          tagName: '',
         },
       ],
       title: data.title,
@@ -76,16 +76,17 @@ const CreatePostPage = () => {
     if (!userInfo.username) {
       setAlert({
         show: true,
-        type: "error",
-        message: "Please login to create post.",
+        type: 'error',
+        message: 'Please login to create post.',
       });
     }
     apiMutate(formData);
     setUploadingFiles([]);
   };
+
   // back button
   const handleClickCreatePostBack = () => {
-    navigate("/posts");
+    navigate('/posts');
   };
 
   const handleFileSelection = async (event) => {
@@ -101,10 +102,10 @@ const CreatePostPage = () => {
       setUploadedFiles((prevFiles) => [...prevFiles, ...successfulUploads]);
       uploadResults.forEach((result) => {
         if (!result.success) {
-          setAlert({ show: true, type: "error", message: result.message });
+          setAlert({ show: true, type: 'error', message: result.message });
         }
       });
-      console.log("all the files uploaded successfully", uploadResults);
+      console.log('all the files uploaded successfully', uploadResults);
       // setAlert({
       //   show: true,
       //   type: "success",
@@ -114,13 +115,15 @@ const CreatePostPage = () => {
     } catch (err) {
       setAlert({
         show: true,
-        type: "error",
-        message: "Failed to upload files.",
+        type: 'error',
+        message: 'Failed to upload files.',
       });
     }
   };
+
   const displayImage =
     selectedFiles.length > 0 ? URL.createObjectURL(selectedFiles[0]) : null;
+
   // file upload
   const handleBrowseFiles = () => {
     fileInputRef.current.click();
@@ -139,23 +142,41 @@ const CreatePostPage = () => {
     setClickedRadio((prevState) => !prevState);
   };
 
+  const renderThumbnails = () => {
+    return selectedFiles.slice(0, 3).map((file, index) => (
+      <img
+        key={index}
+        src={URL.createObjectURL(file)}
+        alt={`Thumbnail ${index + 1}`}
+        style={{
+          maxWidth: '100%',
+          maxHeight: '100%',
+          width: '80px',
+          height: '80px',
+          objectFit: 'contain',
+          marginRight: '10px',
+        }}
+      />
+    ));
+  };
+
   return (
     <div>
       {alert.show && (
         <Alert
           status={alert.type}
-          variant="solid"
+          variant='solid'
           style={{
-            zIndex: "100",
-            position: "fixed",
-            top: "65px",
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-            width: "100%", // Adjust the width as needed
+            zIndex: '100',
+            position: 'fixed',
+            top: '65px',
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            width: '100%',
           }}
         >
-          <div style={{ display: "flex", alignItems: "center" }}>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
             <AlertIcon />
             <AlertDescription>{alert.message}</AlertDescription>
           </div>
@@ -164,30 +185,30 @@ const CreatePostPage = () => {
       )}
       <form
         onSubmit={handleSubmit(onSubmit)}
-        className="create-post-page-container"
+        className='create-post-page-container'
       >
-        <div className="pink-background-1"></div>
-        <div className="pink-background-2"></div>
+        <div className='pink-background-1'></div>
+        <div className='pink-background-2'></div>
 
         <button
-          type="button"
+          type='button'
           onClick={handleClickCreatePostBack}
-          className="create-post-page-back-button-container"
+          className='create-post-page-back-button-container'
         >
           <img
             src={Arrow}
-            alt="Image-Arrow-Icon"
-            className="arrow-back-button"
+            alt='Image-Arrow-Icon'
+            className='arrow-back-button'
           />
-          <span className="create-post-page-back-button">Create a post</span>
+          <span className='create-post-page-back-button'>Create a post</span>
         </button>
 
-        <div className="create-post-page-inner-container">
+        <div className='create-post-page-inner-container'>
           <input
-            type="file"
-            id="imageUpload"
-            accept="image/*"
-            style={{ display: "none" }}
+            type='file'
+            id='imageUpload'
+            accept='image/*'
+            style={{ display: 'none' }}
             onChange={handleFileSelection}
             multiple
           />
@@ -195,19 +216,19 @@ const CreatePostPage = () => {
             <img
               src={displayImage}
               style={{
-                maxWidth: "100%",
-                maxHeight: "100%",
-                width: "330px",
-                height: "330px",
-                objectFit: "contain",
+                maxWidth: '100%',
+                maxHeight: '100%',
+                width: '330px',
+                height: '330px',
+                objectFit: 'contain',
               }}
-              alt="Selected"
+              alt='Selected'
             />
           ) : (
             <>
-              <div className="left-container">
+              <div className='left-container'>
                 <div
-                  className="create-post-page-add"
+                  className='create-post-page-add'
                   onDrop={handleDrop}
                   onDragOver={handleDragOver}
                   onClick={handleBrowseFiles}
@@ -215,86 +236,87 @@ const CreatePostPage = () => {
                   <img
                     src={createPostIcon}
                     style={{
-                      width: "157px",
-                      height: "157px",
+                      width: '157px',
+                      height: '157px',
                     }}
-                    alt="Image-Create-Post"
+                    alt='Image-Create-Post'
                   />
                   <input
-                    type="file"
+                    type='file'
                     ref={fileInputRef}
                     onChange={handleFileSelection}
                     multiple
-                    style={{ display: "none" }}
+                    style={{ display: 'none' }}
                   />
                 </div>
-                <div className="create-post-page-text">
+                <div className='create-post-page-text'>
                   Lorem ipsum dolor sit amet, consectetur adipiscing
                 </div>
               </div>
+              <div className='thumbnail-container'>{renderThumbnails()}</div>
             </>
           )}
 
-          <div className="right-container">
+          <div className='right-container'>
             <div>
               <input
-                type="text"
-                placeholder="Title"
-                className="create-post-page-title"
-                {...register("title", {
-                  required: "* Title is required.",
+                type='text'
+                placeholder='Title'
+                className='create-post-page-title'
+                {...register('title', {
+                  required: '* Title is required.',
                   maxLength: {
                     value: 20,
-                    message: "* Maximum limit for characters is 20.",
+                    message: '* Maximum limit for characters is 20.',
                   },
                 })}
               />
-              <p className="title-error-validation">{errors.title?.message}</p>
+              <p className='title-error-validation'>{errors.title?.message}</p>
 
-              <div className="description-container">
+              <div className='description-container'>
                 <textarea
-                  name="brief"
-                  id="description"
-                  placeholder="Description"
-                  className="create-post-page-description"
-                  {...register("description", {
-                    required: "* Description is required.",
+                  name='brief'
+                  id='description'
+                  placeholder='Description'
+                  className='create-post-page-description'
+                  {...register('description', {
+                    required: '* Description is required.',
                   })}
                 ></textarea>
 
                 <PostDropDownFilter />
                 <PostDropDownFilter />
 
-                <p className="description-error-validation">
+                <p className='description-error-validation'>
                   {errors.description?.message}
                 </p>
               </div>
             </div>
 
-            <div className="wrapper">
+            <div className='wrapper'>
               {/* --- radio button --- */}
-              <div className="create-post-page-radio-button-container">
+              <div className='create-post-page-radio-button-container'>
                 <input
-                  id="input-linked"
-                  type="radio"
-                  name="input-radio-button"
+                  id='input-linked'
+                  type='radio'
+                  name='input-radio-button'
                   checked={clickedRadio}
                   onChange={handleRadioClick}
-                  className="create-post-input-radio-button"
+                  className='create-post-input-radio-button'
                 />
                 <label
-                  htmlFor="input-linked"
-                  className="create-post-input-radio-button-label"
+                  htmlFor='input-linked'
+                  className='create-post-input-radio-button-label'
                 >
                   Restrict my post to viewers over 18
                 </label>
               </div>
 
               {/* --- button --- */}
-              <div className="post-information-sendButton">
+              <div className='post-information-sendButton'>
                 <FormButton
-                  buttonName="Post"
-                  className="create-post-custom-button"
+                  buttonName='Post'
+                  className='create-post-custom-button'
                 />
               </div>
             </div>
