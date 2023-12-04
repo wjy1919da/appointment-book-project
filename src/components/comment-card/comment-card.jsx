9e8e97fe8516f0ae562121b024bd9c44294f7376
Emmy Manning <eobcre@gmail.com>
2023-12-04 14:35:56 -0500

    updated users comment layout in comment card

--- src/components/comment-card/comment-card.jsx
+++ src/components/comment-card/comment-card.jsx
@@ -1,57 +1,66 @@
 import React from 'react';
-import "./comment-card.styles.scss";
+import './comment-card.styles.scss';
 import HeartIcon from '../../assets/post/heart.png';
 import commentIcon from '../../assets/post/chat_bubble.png';
-import '../components-posts/community-post-detail-pop-up/community-post-detail-pop-up.styles.scss'
+import '../components-posts/community-post-detail-pop-up/community-post-detail-pop-up.styles.scss';
 import userInfoQueryStore from '../../userStore.ts';
 
-const CommentCard = ({avatar,name,date,commentText,onClick}) => {
+const CommentCard = ({ avatar, name, date, commentText, onClick }) => {
+  if (!avatar && !name && !date && !commentText) {
+    return null;
+  }
 
-    
-    if(!avatar&&!name&&!date&&!commentText)
-    {
-        return null
-    }
-    
-    const formatDate = (dateString) => {
-        const dateParts = dateString.split('/');
-        const month = dateParts[0];
-        const day = dateParts[1];
-        const formattedDate = `${month}/${day}`;
-        return formattedDate;
-      };
-    const newDate=formatDate(date)
+  const formatDate = (dateString) => {
+    const dateParts = dateString.split('/');
+    const month = dateParts[0];
+    const day = dateParts[1];
+    const formattedDate = `${month}/${day}`;
+    return formattedDate;
+  };
+  const newDate = formatDate(date);
 
-    function convertUnicode(input) {
-        return input.replace(/\\+u([0-9a-fA-F]{4})/g, (a,b) =>
-            String.fromCharCode(parseInt(b, 16)));
-    }
+  function convertUnicode(input) {
+    return input.replace(/\\+u([0-9a-fA-F]{4})/g, (a, b) =>
+      String.fromCharCode(parseInt(b, 16))
+    );
+  }
 
-    return (
-        <div className='comment-card-container'>
-            <div className='reviewer-profile-information'>
-                <div className="reviewer-progile-avatar">
-                    <img src= {avatar} className="reviewer-avatar" alt='avatar'></img>
-                </div>
-                <div className="reviewer-information">
-                    <div className="userName-date">
-                        <span className="detail-gray-font">{name ? convertUnicode(name) : ""}</span>
-                        <span className="detail-comment-text">{commentText ? convertUnicode(commentText) : ""}</span>
-                        <span>12/4/2023</span>
-                        <button>Reply</button>
-                    </div>
-                </div>
+  return (
+    <div className='comment-card-container'>
+      <div className='reviewer-profile-information'>
+        <div className='reviewer-progile-avatar'>
+          <img src={avatar} className='reviewer-avatar' alt='avatar'></img>
+        </div>
+        <div className='reviewer-information'>
+          <div className='userName-date'>
+              <span className='detail-gray-font'>
+                {name ? convertUnicode(name) : ''}
+              </span>
+              <span className='detail-comment-text'>
+                {commentText ? convertUnicode(commentText) : ''}
+              </span>
+            <div className='comment-card-second-line'>
+              <span className='comment-card-date'>12/4/2023</span>
+              <button className='comment-card-button'>Reply</button>
             </div>
-            <div className="likeCount-commentCount">
-                <span>
-                    <img className='post-detail-icon' src ={HeartIcon} alt='like' onClick={onClick}></img>
-                </span>
-                {/* <span>
+          </div>
+        </div>
+      </div>
+      <div className='likeCount-commentCount'>
+        <span>
+          <img
+            className='post-detail-icon'
+            src={HeartIcon}
+            alt='like'
+            onClick={onClick}
+          ></img>
+        </span>
+        {/* <span>
                     <img className='post-detail-icon' src={commentIcon} alt='comment' onClick={onClick}></img>
                 </span> */}
-            </div>
-        </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default CommentCard;
