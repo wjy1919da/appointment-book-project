ee23400e7b0b82f00701f9bd2e6957d5e0d7a796
ItsFrai <fpourooshasb@gmail.com>
2023-12-08 19:04:10 -0500

    buttons

--- src/components/community-post-main-title/community-post-main-title.jsx
+++ src/components/community-post-main-title/community-post-main-title.jsx
@@ -3,7 +3,7 @@ import './community-post-main-title.scss';
 
 const PostPageTitle = () => {
   return (
-    <div className='post-main-title'>
+    <div className='post-main-title' id='community-posts'>
       <h1>Community Posts</h1>
     </div>
   );
