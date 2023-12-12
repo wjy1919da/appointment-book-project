b3c08d9cf7e2fe808c543f9cfce95d006302f3dc
Emmy Manning <eobcre@gmail.com>
2023-12-11 20:21:11 -0500

    modifed by accident

---
+++ src/hooks/useDeletePost.js
@@ -0,0 +1,23 @@
+import { useMutation } from 'react-query';
+import APIClient from '../services/api-client';
+
+export function useDeletePost() {
+  const apiClient = new APIClient('/post/{id}');
+
+  const useDeletePost = useMutation(
+    async (postId) => {
+      const response = await apiClient.delete(postId);
+      return response.data; 
+    },
+    {
+      onSuccess: (data) => {
+        console.log('OK', data);
+      },
+      onError: (error) => {
+        console.error('ERROR', error);
+      },
+    }
+  );
+
+  return useDeletePost;
+}
