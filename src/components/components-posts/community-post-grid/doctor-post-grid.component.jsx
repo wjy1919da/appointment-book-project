f2df302366d37862e98e1292ea2200aa02ce950b
Emmy Manning <eobcre@gmail.com>
2023-12-22 12:13:59 -0500

    implemented like function with new props isLike and manipulating with 2 local states counterLikes and isHeartLiked

--- src/components/components-posts/community-post-grid/doctor-post-grid.component.jsx
+++ src/components/components-posts/community-post-grid/doctor-post-grid.component.jsx
@@ -1,27 +1,27 @@
-import React, { useState, useEffect } from "react";
-import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
-import { Link, useParams, useNavigate } from "react-router-dom";
-import { useMediaQuery } from "react-responsive";
-import usePostQueryStore from "../../../postStore.ts";
+import React, { useState, useEffect } from 'react';
+import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
+import { Link, useParams, useNavigate } from 'react-router-dom';
+import { useMediaQuery } from 'react-responsive';
+import usePostQueryStore from '../../../postStore.ts';
 
 // components
-import CommunityPost from "../community-post/community-post.component";
-import PostDetail from "../community-post-detail/community-post-detail.component";
-import HomeSpinner from "../../home-spinner/home-spinner.component";
-import InfiniteScroll from "react-infinite-scroll-component";
-import ErrorMsg from "../../error-msg/error-msg.component";
+import CommunityPost from '../community-post/community-post.component';
+import PostDetail from '../community-post-detail/community-post-detail.component';
+import HomeSpinner from '../../home-spinner/home-spinner.component';
+import InfiniteScroll from 'react-infinite-scroll-component';
+// import ErrorMsg from "../../error-msg/error-msg.component";
 
 // hook
-import { useApiRequestPostFilter } from "../../../hooks/useApiRequestPostFilter";
-import { useGetLikesPost } from "../../../hooks/useGetPosts";
+import { useApiRequestPostFilter } from '../../../hooks/useApiRequestPostFilter';
+// import { useGetLikesPost } from "../../../hooks/useGetPosts";
 
 // scss
-import "./doctor-post-grid.styles.scss";
+import './doctor-post-grid.styles.scss';
 
 // images
-import Arrow from "../../../assets/post/arrow_grid.png";
-import Arrow1 from "../../../assets/post/arrow1_grid.png";
-import { set } from "date-fns";
+import Arrow from '../../../assets/post/arrow_grid.png';
+import Arrow1 from '../../../assets/post/arrow1_grid.png';
+import { set } from 'date-fns';
 
 // import userInfoQueryStore from '../../../userStore.ts';
 // import Cookie from 'js-cookie';
@@ -38,6 +38,7 @@ const DoctorPostGrid = ({ isAbout }) => {
   const setUserName = usePostQueryStore((state) => state.setUserName);
   const setIsHighlight = usePostQueryStore((state) => state.setIsHighlight);
   const setIsPrivate = usePostQueryStore((state) => state.setIsPrivate);
+  const setIsLike = usePostQueryStore((state) => state.setIsLike);
   const setUserAvatar = usePostQueryStore((state) => state.setUserAvatar);
   // const postQuery = usePostQueryStore((state) => state.postQuery);
   const setMemberID = usePostQueryStore((state) => state.setMemberID);
@@ -45,7 +46,7 @@ const DoctorPostGrid = ({ isAbout }) => {
   // const [title, setTitle] = useState("");
   const flatData = data?.pages?.flatMap((page) => page.data || []) || [];
   const isMobile = useMediaQuery({ query: `(max-width: 1024px)` });
-  const [gutterwidth, setGutterWidth] = useState("");
+  const [gutterwidth, setGutterWidth] = useState('');
   const isMobileOrAbout = isMobile || isAbout;
   // console.log("flatData", flatData);
 
@@ -58,7 +59,8 @@ const DoctorPostGrid = ({ isAbout }) => {
     title,
     memberId,
     isHighlight,
-    isPrivate
+    isPrivate,
+    isLike
   ) => {
     setIsModelOpen(true);
     setPostID(ID);
@@ -68,18 +70,19 @@ const DoctorPostGrid = ({ isAbout }) => {
     setMemberID(memberId);
     setIsHighlight(isHighlight);
     setIsPrivate(isPrivate);
+    setIsLike(isLike);
   };
   if (error) {
-    navigate("*");
+    navigate('*');
   }
   useEffect(() => {
-    setGutterWidth(isMobileOrAbout ? "0px" : "10px");
+    setGutterWidth(isMobileOrAbout ? '0px' : '10px');
   }, [isMobile]);
   if (isLoading) return <HomeSpinner />;
 
   const postCardList = flatData.map((post) => (
     <div
-      className="btn"
+      className='btn'
       onClick={() => {
         handleClickPost(
           post.id,
@@ -88,7 +91,7 @@ const DoctorPostGrid = ({ isAbout }) => {
           post.title,
           post.memberId
         );
-        navigate("/posts/" + post.id);
+        navigate('/posts/' + post.id);
       }}
       key={post.id}
     >
@@ -97,16 +100,17 @@ const DoctorPostGrid = ({ isAbout }) => {
         dummyPrivate={post.isDisplay}
         id={post.id}
         imageURL={post.coverImg || []}
-        text={post.title || ""}
-        profileImage={post.avatar || ""}
-        authorName={post.nickname || ""}
+        text={post.title || ''}
+        profileImage={post.avatar || ''}
+        authorName={post.nickname || ''}
         likes={post.likedCount || 0}
+        liked={post.isLike}
       />
     </div>
   ));
 
   return (
-    <div className="doctor-post-grid-inner-container">
+    <div className='doctor-post-grid-inner-container'>
       {flatData && (
         <InfiniteScroll
           dataLength={flatData.length}
@@ -137,17 +141,17 @@ const DoctorPostGrid = ({ isAbout }) => {
           isMobile={isMobile}
         />
       )}
-      <div className="down-load-more-container">
+      <div className='down-load-more-container'>
         {!isMobile && (
-          <img src={Arrow} alt="arrow" className="arrow-containter" />
+          <img src={Arrow} alt='arrow' className='arrow-containter' />
         )}
         {isMobile && (
-          <img src={Arrow1} alt="arrow1" className="arrow1-containter" />
+          <img src={Arrow1} alt='arrow1' className='arrow1-containter' />
         )}
-        <div className="download-text">Join Charm community to view more</div>
-        <Link to="/download">
-          <button className="download-button">
-            <div className="download-button-text">DownLoad APP</div>
+        <div className='download-text'>Join Charm community to view more</div>
+        <Link to='/download'>
+          <button className='download-button'>
+            <div className='download-button-text'>DownLoad APP</div>
           </button>
         </Link>
       </div>
