90c9916f3de76824c06cd5a7024630e4242668c6
Emmy Manning <eobcre@gmail.com>
2023-12-26 22:21:11 -0500

    responsive design in create a post page and community post input button

--- src/components/components-posts/community-post-search-box/community-post-search-box.jsx
+++ src/components/components-posts/community-post-search-box/community-post-search-box.jsx
@@ -18,53 +18,79 @@ const toUrlParam = (text) => {
   else return text.replace(/_/g, ' ');
 };
 
-const PostSearchBox = ({ value = '', handleSearch, ...otherProps }) => {
+const PostSearchBox = ({ value, handleSearch, isMobile, ...otherProps }) => {
   const [isInputVisible, setIsInputVisible] = useState(false);
 
-  const handleSearchMobile = () => {
+  // small screens search icon button
+  const handleResponsiveButtonClick = () => {
     setIsInputVisible(!isInputVisible);
   };
 
   return (
-    <>
-      {/* desktop */}
-      <div className='community-post-search-box-container'>
-        <input
-          type='text'
-          value={value}
-          {...otherProps}
-          className='community-post-search-box-input'
-        />
-        <button type='button'>
+    <div className={`community-post-search-box-container ${isInputVisible ? 'active' : ''}`}>
+      {isMobile && !isInputVisible && (
+        <button
+          type='button'
+          className='mobile-search-button'
+          style={{
+            textAlign: 'center',
+            border: '1px solid #675D59',
+            borderRadius: '8px',
+            padding: '7px',
+            width: '60px',
+          }}
+          onClick={handleResponsiveButtonClick}
+        >
           <img
             src={SearchIcon}
             alt='Image-Search-Icon'
             className='community-post-search-box-icon'
-            onClick={handleSearch}
+            style={{ display: 'block', margin: '0 auto' }}
           />
         </button>
-      </div>
-      {/* mobile */}
-      <div className='mobile-community-post-search-box-container'>
-        <button type='button' className='mobile-community-post-search-box-button'>
-          <img
-            src={SearchIcon}
-            alt='Image-Search-Icon'
-            className='mobile-community-post-search-box-icon'
-            onClick={handleSearchMobile}
-          />
-        </button>
-        {isInputVisible && (
+      )}
+      {(isInputVisible || !isMobile) && (
+        <div className='search-input-container'>
           <input
             type='text'
             value={value}
             {...otherProps}
-            className='mobile-community-post-search-box-input'
+            className='community-post-search-box-input'
           />
-        )}
-      </div>
-    </>
+          <button type='button' onClick={handleSearch}>
+            <img
+              src={SearchIcon}
+              alt='Image-Search-Icon'
+              className='community-post-search-box-icon'
+            />
+          </button>
+        </div>
+      )}
+    </div>
   );
 };
 
 export default PostSearchBox;
+
+// const PostSearchBox = ({ value = '', handleSearch, ...otherProps }) => {
+//   return (
+//     <div className='community-post-search-box-container'>
+//       <input
+//         type='text'
+//         value={value}
+//         {...otherProps}
+//         className='community-post-search-box-input'
+//       />
+//       <button type='button'>
+//         <img
+//           src={SearchIcon}
+//           alt='Image-Search-Icon'
+//           className='community-post-search-box-icon'
+//           onClick={handleSearch}
+//         />
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default PostSearchBox;
