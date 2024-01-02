53f1d4b73527d0a1d5282392c045bcd05fed962e
Emmy Manning <eobcre@gmail.com>
2024-01-01 23:13:22 -0500

    fixed doctor profile image and edit profile button responsive logic and style

--- src/components/doctor-own-profile/doctor-own-profile-edit-button.jsx
+++ src/components/doctor-own-profile/doctor-own-profile-edit-button.jsx
@@ -1,7 +1,9 @@
 import "./doctor-own-profile-Basic.styles.scss";
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+
 const DoctorOwnProfileEditButton = ({ onClick, title }) => {
+
   return (
     <div className="top-edit-button-1" onClick={onClick}>
       {title}
