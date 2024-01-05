0f64b96af399857648399193961f9d01afa3996c
Emmy Manning <eobcre@gmail.com>
2024-01-05 11:00:20 -0500

    updated contents in appointment tab

--- src/components/doctor-appointment-profile/tabs/doctor-appointment-profile-appointment.jsx
+++ src/components/doctor-appointment-profile/tabs/doctor-appointment-profile-appointment.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 // components
 import Button from '../../components-posts/community-post-button/community-post-button';
+// dummy data
+import { appointmentData } from '../data/appointmentData';
 
 // scss
 import './doctor-appointment-profile-appointment.scss';
@@ -27,6 +29,47 @@ const DoctorAppointmentProfileAppointmentTab = () => {
           <p className='doctor-appointment-profile-appointment-tab-date-title'>
             Friday, 6 January 2023
           </p>
+          <div>
+            {appointmentData.map((item, index) => (
+              <div
+                key={index}
+                className='doctor-appointment-profile-appointment-tab-list-container'
+              >
+                <span className='doctor-appointment-profile-appointment-tab-time'>
+                  {item.time}
+                </span>
+                {/* <span>{item.src}</span> */}
+                <span className='doctor-appointment-profile-appointment-tab-await'>
+                  {item.await}
+                </span>
+                <div className='doctor-appointment-profile-appointment-tab-tag-container'>
+                  {item.tag1 && (
+                    <span className='doctor-appointment-profile-appointment-tab-tag-1'>
+                      {item.tag1}
+                    </span>
+                  )}
+                  {item.tag2 && (
+                    <span className='doctor-appointment-profile-appointment-tab-tag-2'>
+                      {item.tag2}
+                    </span>
+                  )}
+                </div>
+                <span
+                  className={`${
+                    item.status === 'Open Slot'
+                      ? 'doctor-appointment-profile-appointment-tab-status-open-slot'
+                      : item.status === 'Close Slot'
+                      ? 'doctor-appointment-profile-appointment-tab-status-close-slot'
+                      : item.status === 'Confirm'
+                      ? 'doctor-appointment-profile-appointment-tab-status-confirm'
+                      : ''
+                  }`}
+                >
+                  {item.status}
+                </span>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
