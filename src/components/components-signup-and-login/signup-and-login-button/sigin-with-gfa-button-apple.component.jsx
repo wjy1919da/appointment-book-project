bb59ebd5db6677081b97b058992a040f65b9497d
Fraidoon Pourooshasb <fpourooshasb@gmail.com>
2024-01-20 19:46:11 -0500

    apple and facebook

--- src/components/components-signup-and-login/signup-and-login-button/sigin-with-gfa-button-apple.component.jsx
+++ src/components/components-signup-and-login/signup-and-login-button/sigin-with-gfa-button-apple.component.jsx
@@ -1,23 +1,50 @@
-import AppleLogin from "react-apple-login";
+import React from 'react';
+import AppleLogin from 'react-apple-login';
+import { useAppleLogin } from '../../../hooks/useAuth';
+import { useToast } from '@chakra-ui/react';
+import SignupAndLoginButton from './signup-and-login-button.component';
 
-import SignupAndLoginButton from "./signup-and-login-button.component";
+const AppleLoginBtnWrap = (props) => {
+    const appleSignin = useAppleLogin();
+    const toast = useToast();
 
-//N8YQ2J6D7H (Team ID)
-// Key ID：F28BBAKJ84
-// clientId :com.longcai.charm
-const AppleLoginBtnWrap = (props) => (
-    <AppleLogin
-        usePopup={false}
-        clientId="com.longcai.charm.sign"
-        redirectURI="https://local.test:3000"
-        // redirectURI="https://localhost:3000"
-        render={(
-            renderProps //Custom Apple Sign in Button
-        ) => {
-            // props.children.onClick = renderProps.onClick
-            return <SignupAndLoginButton {...props} onClick={renderProps.onClick}></SignupAndLoginButton>;
-        }}
-    />
-);
+    const handleAppleLoginSuccess = async (response) => {
+        console.log('Apple login successful:', response);
+        const token = response.token;
+
+        try {
+            const result = await appleSignin.mutateAsync({ token });
+
+            if (result.status === 'success') {
+                toast.success('Login successful!');
+            } else {
+                toast.error(result.message || 'Login failed. Please try again.');
+            }
+        } catch (error) {
+            console.log("Backend error or some other error : ", error);
+        }
+    };
+
+    const handleAppleLoginFailure = (error) => {
+        console.error('Apple login failed:', error);
+    };
+
+    return (
+        <AppleLogin
+            usePopup={false}
+            clientId="com.longcai.charm.sign"
+            redirectURI="https://local.test:3000"
+            responseMode="query"
+            //responseType="code id_token"
+            handleSuccess={handleAppleLoginSuccess}
+            handleFailure={handleAppleLoginFailure}
+            scope="email phone"
+            render={({ onClick }) => (
+                <SignupAndLoginButton {...props} onClick={onClick}></SignupAndLoginButton>
+            )}
+        />
+    );
+};
 
 export default AppleLoginBtnWrap;
+
