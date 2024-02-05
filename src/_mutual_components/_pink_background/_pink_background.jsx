3d511de7150f965dc0434a0f4e85f60d2df13980
Emmy Manning <emswoo@EM.local>
2024-02-05 12:17:01 -0500

    created a new _pink_background folder

---
+++ src/_mutual_components/_pink_background/_pink_background.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './_pink_background.scss';
+
+const PinkBackgroundComponent = () => {
+  return (
+    <>
+      <div className='pink-background-1'></div>
+      <div className='pink-background-2'></div>
+    </>
+  );
+};
+
+export default PinkBackgroundComponent;
