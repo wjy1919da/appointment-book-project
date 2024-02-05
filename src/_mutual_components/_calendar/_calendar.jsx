7240a84de9bed1cb9a1f1b1f739f48aff0d3728e
Emmy Manning <emswoo@EM.local>
2024-02-05 11:42:36 -0500

    created mutual components folder

---
+++ src/_mutual_components/_calendar/_calendar.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import './_calendar.scss'
+
+const CalendarComponent = () => {
+  function isToday(date) {
+    return new Date().toDateString() === date.toDateString();
+  }
+
+  const [date, setDate] = useState(new Date()); // react-calendar date
+
+  const handleChangeDate = (newDate) => {
+    setDate(newDate);
+  };
+
+  const tileClassName = ({ date }) => {
+    const isToday =
+      date.getDate() === new Date().getDate() &&
+      date.getMonth() === new Date().getMonth() &&
+      date.getFullYear() === new Date().getFullYear();
+
+    return isToday ? 'today-tile' : '';
+  };
+
+  // change the week day format to two letters
+  const formatShortWeekday = (locale, date) => {
+    // date = new Date();
+    const weekdays = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
+    return weekdays[date.getDay()];
+  };
+
+  return (
+    <>
+      <Calendar
+        onChange={handleChangeDate}
+        value={date}
+        locale='en-GB'
+        formatShortWeekday={formatShortWeekday}
+        tileClassName={tileClassName}
+        tileContent={({ date, view }) => {
+          if (view === 'month') {
+            if (isToday(date)) {
+              return <div className='is-today'></div>;
+            }
+          }
+        }}
+      />
+    </>
+  );
+};
+
+export default CalendarComponent;
