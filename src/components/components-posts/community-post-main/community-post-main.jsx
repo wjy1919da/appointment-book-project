798ba059c857f339f84c25601b1c84ad1a2418cf
Fraidoon Pourooshasb <fpourooshasb@gmail.com>
2024-02-09 15:40:06 -0500

    changes to ui

--- src/components/components-posts/community-post-main/community-post-main.jsx
+++ src/components/components-posts/community-post-main/community-post-main.jsx
@@ -25,31 +25,16 @@ const PostPageMain = () => {
     }
   };
   return (
-    <div>
-      <div className="post-main-container-wrapper">
-        <div className="post-main-container">
-          <div className="post-main-shape">
-            <div className="wrapper">
-              <div className="post-main-photograph"></div>
-            </div>
-            <span className="occaecat">#Occaecat</span>
-            <span className="adipiscing">#Adipiscing</span>
-            <div className="wrapper">
-              <span className="lorem">#Lorem</span>
-            </div>
-            <span className="eiusmod">#Eiusmod</span>
-            <span className="excepteur">#Excepteur</span>
-            <img src={PhotoGirl} alt="PhotoGraph-1" className="photo-girl" />
-            <img src={Photo} alt="Photograph-1" className="photo-1" />
-            <img src={Photo} alt="Photograph-2" className="photo-2" />
-            <p className="post-main-photograph-caption">
-              Top Makeup Trends for Spring 2023
-            </p>
-            <div className="post-main-likes-container">
-              <img src={Heart} alt="Heart-Icon" className="heart-icon" />
-              <p className="post-main-photograph-like">1.8k</p>
-            </div>
-          </div>
+    <div className='community-post-main-container'>
+      {/* left container */}
+      <div className='community-post-main-left-container'>
+        <PinkBackgroundComponent renderBothBackgrounds={false} />
+        <img
+          src={GirlPhotoMain}
+          alt='Image'
+          className='community-post-main-girl-image'
+        />
+      </div>
 
       {/* right container */}
       <div className='community-post-main-right-container'>
@@ -78,9 +63,10 @@ const PostPageMain = () => {
               <br />
               empowerment
             </h1>
-            <h6 className="h6val">
-              Charm Life lets you share your cosmetic experience with others and
-              stay on-trend
+            <h6 className='community-post-main-sub-title'>
+              Charm Life lets you share your cosmetic
+              <br />
+              experience with others and stay on-trend
             </h6>
           </>
         )}
@@ -92,7 +78,7 @@ const PostPageMain = () => {
               className='community-post-main-create-button'
               onClick={handleCreatePostClick}
             >
-              Creating a post
+              <span className='create-post-text'>Creating a post</span>
               <img
                 src={ArrowRight}
                 alt='ArrowRight'
